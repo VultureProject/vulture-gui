726a15a8992b0d150029d66bd3a1bc8a7bd89d11
Kevin Guillemot <kevin.guillemot@advens.fr>
2019-09-25 14:59:57 +0200

    UPGRADE :: Fixes : Fix some problems in upgrade routine

--- vulture_os/toolkit/updates/v0.9.91/reset_darwin_filters.sh
+++ vulture_os/toolkit/updates/v0.9.91/reset_darwin_filters.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
 # If it is the master mongo
+/bin/echo -n "Verifying if the current node is master : "
 if /usr/sbin/jexec mongodb /usr/local/bin/mongo --quiet --ssl --sslCAFile /var/db/pki/ca.pem --sslPEMKeyFile /var/db/pki/node.pem "`hostname`:9091/vulture" --eval "db.isMaster()['ismaster']" | /usr/bin/grep "true" ; then
     /usr/local/bin/sudo -u vlt-os /home/vlt-os/env/bin/python3.6 /home/vlt-os/vulture_os/manage.py loaddata filters
+else
+    echo "false"
 fi
