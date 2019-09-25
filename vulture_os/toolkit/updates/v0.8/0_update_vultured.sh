592cc958a7f4259e8783efb89c068fc686c0f046
Kevin Guillemot <kevin.guillemot@advens.fr>
2019-09-25 14:31:54 +0200

    UPGRADE :: Fixes : Fix some problems in upgrade routine

--- vulture_os/toolkit/updates/v0.8/0_update_vultured.sh
+++ vulture_os/toolkit/updates/v0.8/0_update_vultured.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
-/bin/kill -9 $(/bin/cat /var/run/vulture/vultured.pid)
+if [ -f /var/run/vulture/vultured.pid ] ; then
+    /bin/kill -9 $(/bin/cat /var/run/vulture/vultured.pid)
+fi
 
 /bin/echo "[!] Please start vultured at the end of the update [!]"
