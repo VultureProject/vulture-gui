9885c717d959dbf765a5959df78ff23b87955fb7
Kevin Guillemot <kevin.guillemot@advens.fr>
2019-09-26 13:57:14 +0200

    UPGRADE :: Fix : Fix some problems in 0.9.91 upgrade files

--- vulture_os/toolkit/updates/v0.9.91/create_node_ok.sh
+++ vulture_os/toolkit/updates/v0.9.91/create_node_ok.sh
@@ -2,6 +2,7 @@
 
 # Add the file /home/vlt-os/vulture_os/.node_ok if the node has already been bootstrapped (cluster_create or cluster_join)
 
-if [ -f /home/vlt-os/.install ] ; then
-    /bin/mv /home/vlt-os/.install /home/vlt-os/vulture_os/.node_ok
+# If the node has been bootstrapped, the rsyslog configuration file has been created
+if [ -f /usr/local/etc/rsyslog.d/pf.conf ] ; then
+    /usr/bin/touch /home/vlt-os/vulture_os/.node_ok
 fi
