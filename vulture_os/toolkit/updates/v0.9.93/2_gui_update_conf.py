fb5a0228d48e3c880a67a03a0cbfa2e121a9de3d
Kevin Guillemot <kevin.guillemot@advens.fr>
2019-10-09 11:25:47 +0200

    UPGRADE :: Omission : Reload gui config in upgrade routine, because template has been improved (ProxyPassMatch added)

--- vulture_os/toolkit/updates/v0.9.93/2_gui_update_conf.py
+++ vulture_os/toolkit/updates/v0.9.93/2_gui_update_conf.py
@@ -43,5 +43,5 @@ if __name__ == "__main__":
     if not node:
         print("Current node not found. Maybe the cluster has not been initiated yet.")
     else:
-        node.api_request("services.darwin.darwin.build_conf")
-        print("1_darwin_update_conf done.")
+        node.api_request("services.apache.apache.reload_conf")
+        print("2_gui_update_conf done.")
