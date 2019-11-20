3921a575292984f2fdd0bb7286273f4798023cea
Kevin Guillemot <kevin.guillemot@advens.fr>
2019-11-20 17:49:53 +0100

    UPGRADE :: Fix : Set gateway_enable & ipv6_gateway_enable to YES in upgrade script

---
+++ vulture_os/toolkit/updates/v1.0/3_set_gateway_enable.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+# Set gateway_enable & ipv6_gateway_enable to YES in /etc/rc.conf.d/routing
+/usr/bin/sed -i '' "s/gateway_enable=.*/gateway_enable=\"YES\"/" /etc/rc.conf.d/routing
+/usr/bin/sed -i '' "s/ipv6_gateway_enable=.*/ipv6_gateway_enable=\"YES\"/" /etc/rc.conf.d/routing
