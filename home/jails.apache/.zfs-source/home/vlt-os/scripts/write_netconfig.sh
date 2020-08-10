2d67493287e7a6c624559fb8457694ab7642665c
Jérémie <jeremie.jourdin@gmail.com>
2020-08-10 17:50:21 +0200

    Always write tap0 configuration in rc.conf.network

--- home/jails.apache/.zfs-source/home/vlt-os/scripts/write_netconfig.sh
+++ home/jails.apache/.zfs-source/home/vlt-os/scripts/write_netconfig.sh
@@ -19,13 +19,19 @@ if [ "$loop" ==  "0" ]; then
         echo "ifconfig_lo0=\"inet 127.0.0.1 netmask 255.255.255.0\"" >  /etc/rc.conf.d/network
         echo "ifconfig_lo0_ipv6=\"inet6 ::1 prefixlen 128\"" >> /etc/rc.conf.d/network
         echo "ifconfig_lo0_alias0=\"inet6 fd00::201 prefixlen 128\"" >> /etc/rc.conf.d/network
-        echo "#Declare LACP and VLAN interfaces (eg: 'lagg0 vlan100 vlan200') in /usr/local/etc/cloned.intf" >> /etc/rc.conf.d/network 
+        echo "# Declare interfaces to be cloned (eg: 'lagg0 vlan100 vlan200') in /usr/local/etc/cloned.intf" >> /etc/rc.conf.d/network 
         echo -n "cloned_interfaces=\"lo1 lo2 lo3 lo4 lo5 lo6${vlan_nic}" >> /etc/rc.conf.d/network
-        if ! /usr/local/sbin/vm switch info public ; then
+
+        # tap0 must be always written in network config to avoid interface drop when altering network settings
+        # if ! /usr/local/sbin/vm switch info public ; then
             echo " tap0\"" >> /etc/rc.conf.d/network
             echo "ifconfig_tap0=\"inet 192.168.1.1 netmask 255.255.255.0\"" >> /etc/rc.conf.d/network
-        else
-            echo "\"" >> /etc/rc.conf.d/network
+        #else
+        #    echo "\"" >> /etc/rc.conf.d/network
+        #fi
+        echo "# Declare custom configuration in /usr/local/etc/custom.intf" >> /etc/rc.conf.d/network
+        if [ -f "/usr/local/etc/custom.intf" ]; then
+            /bin/cat /usr/local/etc/custom.intf >> /etc/rc.conf.d/network
         fi
         echo $config | sed -e 's/_EOL/\
 /g' >> /etc/rc.conf.d/$destination
