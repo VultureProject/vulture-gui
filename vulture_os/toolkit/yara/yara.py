f740c0df420618e55b3adaa229f5a5c03a71e4e0
Théo Bertin <bertintheo@orange.fr>
2020-11-19 13:58:49 +0100

    YARA TOOLKIT:: IMPROVE AND ADD API CALL TO COMPILE/CHECK ALL EXISTING RULES

--- vulture_os/toolkit/yara/yara.py
+++ vulture_os/toolkit/yara/yara.py
@@ -25,6 +25,7 @@ __doc__ = 'System Utils Yara Toolkit'
 # Django project imports
 from darwin.inspection.models import InspectionPolicy, InspectionRule, PACKET_INSPECTION_TECHNO, DEFAULT_YARA_CATEGORIES
 from django.core.exceptions import ObjectDoesNotExist
+from system.exceptions import VultureSystemConfigError
 
 import subprocess
 from toolkit.network.network import get_proxy
@@ -40,10 +41,14 @@ logger = logging.getLogger('system')
 def fetch_yara_rules(logger):
     logger.info("getting updated yara rules...")
 
-    proxy = get_proxy()
+    # proxy = get_proxy()
     try:
         doc_uri = "https://github.com/Yara-Rules/rules/archive/master.zip"
-        doc = requests.get(doc_uri, proxies=proxy)
+        # doc = requests.get(doc_uri, proxies=proxy, timeout=10)
+        doc = requests.get(doc_uri, timeout=10)
+    except requests.Timeout:
+        logger.error("Yara::fetch_yara_rules:: timed out while trying to connect")
+        raise
     except Exception as e:
         logger.error("Yara::fetch_yara_rules:: {}".format(e), exc_info=1)
         raise
@@ -74,7 +79,7 @@ def fetch_yara_rules(logger):
                                 continue
                     if contains_rules:
                         try:
-                            subprocess.check_output(["/usr/local/bin/yara", fullpath, fullpath])
+                            subprocess.check_output(["/usr/local/bin/yara", fullpath, fullpath], stderr=subprocess.PIPE)
                             with open(fullpath, 'r', encoding='utf-8') as content_file:
                                 filtered_lines = [line for line in content_file if "import " not in line]
                                 rule, created = InspectionRule.objects.get_or_create(
@@ -163,3 +168,29 @@ def try_compile_yara_rules(logger, policy_id):
     policy.compile_status = ''
     policy.save_policy_file()
     policy.save()
+
+
+def compile_all_rules(logger):
+    logger.info("Yara::compile_all_rules:: trying to compile all rules")
+    for policy in InspectionPolicy.objects.all():
+        try:
+            filepath = policy.get_full_test_filename()
+
+            with open(filepath, "w", encoding="utf-8") as f:
+                f.write(policy.generate_content())
+
+            subprocess.check_output(["/usr/local/bin/yara", filepath, filepath], stderr=subprocess.STDOUT)
+            logger.info("Yara::compile_all_rules:: inspection policy {} successfuly compiled, writing file".format(policy))
+            policy.compilable = "OK"
+            policy.compile_status = ''
+            policy.save()
+            policy.save_policy_file()
+        except VultureSystemConfigError as e:
+            logger.error("Yara::try_compile_yara_rules:: error while saving file: {}".format(e))
+        except subprocess.CalledProcessError as e:
+            logger.error("Yara::try_compile_yara_rules:: could not compile rules !")
+            policy.compilable = "KO"
+            policy.compile_status = e.output.decode('utf-8')
+            policy.save()
+        except Exception as e:
+            logger.error("Yara::try_compile_yara_rules:: {}".format(e))
