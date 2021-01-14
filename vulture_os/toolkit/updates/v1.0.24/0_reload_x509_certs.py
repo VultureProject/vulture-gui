b2a8303ce74e042fc2808d7749de526a47f2fa9a
Théo Bertin <bertintheo@orange.fr>
2021-01-14 10:53:14 +0100

    UPDATE_SCRIPTS::1.0.24:: ADD SCRIPT TO UPDATE X509 CERTIFICATES

---
+++ vulture_os/toolkit/updates/v1.0.24/0_reload_x509_certs.py
@@ -0,0 +1,65 @@
+#!/home/vlt-os/env/bin/python
+
+"""This file is part of Vulture 4.
+
+Vulture 4 is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Vulture 4 is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Vulture 4.  If not, see http://www.gnu.org/licenses/.
+"""
+__author__ = "Théo Bertin"
+__credits__ = []
+__license__ = "GPLv3"
+__version__ = "4.0.0"
+__maintainer__ = "Vulture Project"
+__email__ = "contact@vultureproject.org"
+__doc__ = 'Reload conf of api logs frontends, after improving templates'
+
+import sys
+import os
+
+if not os.path.exists("/home/vlt-os/vulture_os/.node_ok"):
+    sys.exit(0)
+
+# Django setup part
+sys.path.append('/home/vlt-os/vulture_os')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", 'vulture_os.settings')
+
+import django
+from django.conf import settings
+django.setup()
+
+from system.cluster.models import Cluster, Node
+from system.pki.models import X509Certificate
+
+if __name__ == "__main__":
+
+    node = Cluster.get_current_node()
+    if not node:
+        print("Current node not found. Maybe the cluster has not been initiated yet.")
+    else:
+        try:
+            certificates = X509Certificate.objects.all()
+        except Exception as e:
+            print("Cannot retrieve existing certificates from database: {}".format(str(e)))
+            sys.exit(0)
+
+        for cert in certificates:
+            print("reloading certificate files for '{}'".format(cert))
+            try:
+                cert.save()
+            except Exception as e:
+                print("error while reloading a certificate: {}".format(e))
+
+        print("reloading Haproxy configuration...")
+        node.api_request("services.haproxy.haproxy.reload_service")
+
+        print("Done.")
