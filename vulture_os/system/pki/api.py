17a7f3b7a2516ea450a1480c5b7fec95ac117dd5
Olivier de Régis <olivier@deregis.fr>
2021-04-01 12:07:31 +0200

    Fix TLSProfile/PKI API endpoints

--- vulture_os/system/pki/api.py
+++ vulture_os/system/pki/api.py
@@ -22,12 +22,18 @@ __maintainer__ = "Vulture OS"
 __email__ = "contact@vultureproject.org"
 __doc__ = 'PKI API'
 
-from django.core.exceptions import ObjectDoesNotExist
 from django.views.decorators.http import require_http_methods
 from django.http import HttpResponseForbidden, JsonResponse
+from system.pki.models import X509Certificate, TLSProfile
+from django.core.exceptions import ObjectDoesNotExist
+from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
+from django.utils.translation import gettext_lazy as _
 from gui.decorators.apicall import api_need_key
-from system.pki.models import X509Certificate
+from system.pki.views import tls_profile_edit
+from django.views.generic.base import View
+from django.conf import settings
+from bson import ObjectId
 import logging
 
 logger = logging.getLogger('system')
@@ -70,3 +76,122 @@ def pki_issue_cert(request):
         return JsonResponse(bundle)
     else:
         return HttpResponseForbidden("Error when issuing Node's certificate")
+
+
+@method_decorator(csrf_exempt, name="dispatch")
+class PKIView(View):
+    @api_need_key("cluster_api_key")
+    def get(self, request):
+        try:
+            object_id = request.GET.get('object_id')
+            name = request.GET.get('name')
+
+            if object_id:
+                ret = X509Certificate.objects.get(pk=object_id).to_dict()
+            elif name:
+                ret = X509Certificate.objects.get(name=name).to_dict()
+            else:
+                ret = [p.to_dict() for p in X509Certificate.objects.all()]
+            
+            return JsonResponse({
+                "data": ret
+            })
+
+        except X509Certificate.DoesNotExist:
+            return JsonResponse({
+                "error": _("Object does not exist")
+            }, status=404)
+        
+        except Exception as e:
+            logger.critical(e, exc_info=1)
+            error = _("An error has occurred")
+            if settings.DEV_MODE:
+                error = str(e)
+            
+            return JsonResponse({
+                "error": error
+            }, status=500)
+
+@method_decorator(csrf_exempt, name="dispatch")
+class TLSProfileApiView(View):
+    @api_need_key("cluster_api_key")
+    def get(self, request):
+        try:
+            object_id = request.GET.get('object_id')
+            name = request.GET.get('name')
+
+            if object_id:
+                ret = TLSProfile.objects.get(pk=object_id).to_dict()
+            elif name:
+                ret = TLSProfile.objects.get(name=name).to_dict()
+            else:
+                ret = [p.to_dict() for p in TLSProfile.objects.all()]
+            
+            return JsonResponse({
+                "data": ret
+            })
+
+        except TLSProfile.DoesNotExist:
+            return JsonResponse({
+                "error": _("Object does not exist")
+            }, status=404)
+        
+        except Exception as e:
+            logger.critical(e, exc_info=1)
+            error = _("An error has occurred")
+            if settings.DEV_MODE:
+                error = str(e)
+            
+            return JsonResponse({
+                "error": error
+            }, status=500)
+        
+    @api_need_key('cluster_api_key')
+    def put(self, request, object_id):
+        try:
+            return tls_profile_edit(request, object_id, api=True)
+        except TLSProfile.DoesNotExist:
+            return JsonResponse({
+                "error": _("Object does not exist")
+            }, status=404)
+        
+        except Exception as err:
+            logger.critical(err, exc_info=1)
+            error = _("An error has occurred")
+            if settings.DEV_MODE:
+                error = str(err)
+            
+            return JsonResponse({
+                "error": error
+            }, status=500)
+
+    @api_need_key('cluster_api_key')
+    def post(self, request):
+        try:
+            return tls_profile_edit(request, api=True)
+
+        except Exception as err:
+            logger.critical(err, exc_info=1)
+            error = _("An error has occurred")
+
+            if settings.DEV_MODE:
+                error = str(err)
+            
+            return JsonResponse({
+                "error": error
+            }, status=500)
+     
+    @api_need_key("cluster_api_key")
+    def delete(self, request, object_id):
+        try:
+            obj = TLSProfile.objects.get(pk=ObjectId(object_id))
+            obj.delete()
+
+            return JsonResponse({
+                "status": True
+            }, status=204)
+        except TLSProfile.DoesNotExist:
+            return JsonResponse({
+                "status": False,
+                "error": _("Object does not exists")
+            }, status=404)
