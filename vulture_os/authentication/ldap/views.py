10c3fa796faab24daaf564740927f55cb132f6ab
Olivier de Régis <olivier@deregis.fr>
2021-04-09 08:56:54 +0200

    Fix IDP View

--- vulture_os/authentication/ldap/views.py
+++ vulture_os/authentication/ldap/views.py
@@ -71,9 +71,6 @@ def ldap_clone(request, object_id):
 
     return render(request, 'authentication/ldap_edit.html', {'form': form})
 
-def ldap_view(request, object_id):
-    return render(request, 'authentication/ldap_view.html', {"object_id": object_id})
-
 
 def ldap_edit(request, object_id=None, api=False):
     ldap = None
