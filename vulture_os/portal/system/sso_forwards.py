2c4c8bbb64ee43b10c3666e6f3c5b1f16bfb1bb3
Kevin Guillemot <kevin.guillemot@advens.fr>
2021-04-15 17:59:20 +0200

    SSO-Forward :: Fix : Fix redirection of user to main application page if 'SSO forward follow redirect' disabled

--- vulture_os/portal/system/sso_forwards.py
+++ vulture_os/portal/system/sso_forwards.py
@@ -179,14 +179,14 @@ class SSOForward(object):
             final_response['Content-Length'] = len(final_response.content)
             logger.debug("SSOForward::generate_response: sso_forward_return_post activated - final response generated")
 
-        # elif response.status_code not in (301,302,303) or not self.application.authentication.sso_forward_follow_redirect:
-        #     """ simply redirect to the default Application entry point """
-        #     final_response.status_code = 302
-        #     # redirect user to url redirected
-        #     final_response['Location'] = asked_url
-        #     del final_response['Content-Length']
-        #     del final_response['Content-Encoding']
-        #     logger.debug("SSOForward::generate_response: Generated response redirects to '{}'".format(asked_url))
+        elif response.status_code not in (301,302,303) or not self.application.authentication.sso_forward_follow_redirect:
+            """ simply redirect to the default Application entry point """
+            final_response.status_code = 302
+            # redirect user to url redirected
+            final_response['Location'] = redirect_url
+            del final_response['Content-Length']
+            del final_response['Content-Encoding']
+            logger.debug("SSOForward::generate_response: Generated response redirects to '{}'".format(redirect_url))
 
         return final_response
 
