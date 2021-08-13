5683b8bdfc27a02fe2329087be60267091387494
Théo Bertin <bertintheo@orange.fr>
2021-08-13 13:08:11 +0200

    Revert "UPGRADE::ADDED:: script to remove python37 from system"
    This reverts commit 90b11e74d88d38257c4afd103cbd865834fd6eae.

--- vulture_os/toolkit/updates/v1.3.5/1_remove_python37.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# lock packages depending on python to avoid uninstalling them in case of conflict
-/usr/sbin/pkg lock -y darwin vulture-gui vulture-base
-
-/usr/sbin/pkg remove -y python37
-
-# unlock the packages
-/usr/sbin/pkg unlock -y darwin vulture-gui vulture-base
