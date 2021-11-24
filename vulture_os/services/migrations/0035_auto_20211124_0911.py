464f9b75079e204873ce4452cd13b0ccb1ee2872
Hugo Soszynski <hugo.soszynski@outlook.com>
2021-11-24 23:09:23 +0100

    Removed AlterField becuse concerned field is overwrittent at page loading

--- vulture_os/services/migrations/0035_auto_20211124_0911.py
+++ vulture_os/services/migrations/0035_auto_20211124_0911.py
@@ -40,9 +40,4 @@ class Migration(migrations.Migration):
             name='vadesecure_password',
             field=models.TextField(default='', help_text='Password used to fetch the token for the Vadesecure API', verbose_name='Vadesecure password'),
         ),
-        migrations.AlterField(
-            model_name='frontend',
-            name='filebeat_module',
-            field=models.TextField(choices=[('_custom', 'Custom Filebeat config'), ('activemq', 'Activemq'), ('aws', 'Aws'), ('awsfargate', 'Awsfargate'), ('azure', 'Azure'), ('barracuda', 'Barracuda'), ('bluecoat', 'Bluecoat'), ('cef', 'Cef'), ('checkpoint', 'Checkpoint'), ('cisco', 'Cisco'), ('coredns', 'Coredns'), ('crowdstrike', 'Crowdstrike'), ('cyberark', 'Cyberark'), ('cyberarkpas', 'Cyberarkpas'), ('cylance', 'Cylance'), ('envoyproxy', 'Envoyproxy'), ('f5', 'F5'), ('fortinet', 'Fortinet'), ('gcp', 'Gcp'), ('google_workspace', 'Google_workspace'), ('googlecloud', 'Googlecloud'), ('gsuite', 'Gsuite'), ('ibmmq', 'Ibmmq'), ('imperva', 'Imperva'), ('infoblox', 'Infoblox'), ('iptables', 'Iptables'), ('juniper', 'Juniper'), ('microsoft', 'Microsoft'), ('misp', 'Misp'), ('mssql', 'Mssql'), ('mysqlenterprise', 'Mysqlenterprise'), ('netflow', 'Netflow'), ('netscout', 'Netscout'), ('o365', 'O365'), ('okta', 'Okta'), ('oracle', 'Oracle'), ('panw', 'Panw'), ('proofpoint', 'Proofpoint'), ('rabbitmq', 'Rabbitmq'), ('radware', 'Radware'), ('snort', 'Snort'), ('snyk', 'Snyk'), ('sonicwall', 'Sonicwall'), ('sophos', 'Sophos'), ('squid', 'Squid'), ('suricata', 'Suricata'), ('threatintel', 'Threatintel'), ('tomcat', 'Tomcat'), ('zeek', 'Zeek'), ('zookeeper', 'Zookeeper'), ('zoom', 'Zoom'), ('zscaler', 'Zscaler')], default='tcp', help_text='Filebeat built-in module'),
-        ),
     ]
