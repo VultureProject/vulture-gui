eb862e653b3e3eab7a8e8e7abc5292715dca2e81
Gaultier Parain <gaultier.parain@advens.fr>
2022-06-24 16:18:13 +0200

    add waf_cloudflare.py

--- vulture_os/toolkit/api_parser/waf_cloudflare/waf_cloudflare.py
+++ vulture_os/toolkit/api_parser/waf_cloudflare/waf_cloudflare.py
@@ -14,21 +14,20 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with Vulture OS.  If not, see http://www.gnu.org/licenses/.
 """
-__author__ = "tbertin"
+__author__ = "gparain"
 __credits__ = []
 __license__ = "GPLv3"
 __version__ = "4.0.0"
 __maintainer__ = "Vulture OS"
 __email__ = "contact@vultureproject.org"
-__doc__ = 'Vadesecure O365 API Parser'
-__parser__ = 'VADESECURE O365'
+__doc__ = 'Waf Cloudflare API Parser'
+__parser__ = 'WAF CLOUDFLARE'
 
 import json
 import logging
 import requests
 
-from datetime import timezone
-from datetime import datetime
+from datetime import datetime, timezone, timedelta
 from django.conf import settings
 from django.utils import dateparse
 import time
@@ -40,200 +39,88 @@ logger = logging.getLogger('api_parser')
 
 
 
-class VadesecureO365APIError(Exception):
+class WAFCloudflareAPIError(Exception):
     pass
 
 
-class VadesecureO365Parser(ApiParser):
-    TOKEN_ENDPOINT = "https://api.vadesecure.com/oauth2/v2/token"
-
+class WAFCloudflareParser(ApiParser):
 
     HEADERS = {
         "Content-Type": "application/json",
-        'Accept': 'application/json',
     }
 
     def __init__(self, data):
         """
-            vadesecure_o365_host
-            vadesecure_o365_tenant
-            vadesecure_o365_client_id
-            vadesecure_o365_client_secret
+            waf_cloudflare_zoneid
+            waf_cloudflare_apikey
+
         """
         super().__init__(data)
 
-        self.vadesecure_o365_host = data["vadesecure_o365_host"].rstrip("/")
-        if not self.vadesecure_o365_host.startswith('https://'):
-            self.vadesecure_o365_host = f"https://{self.vadesecure_o365_host}"
+        self.waf_cloudflare_zoneid = data["waf_cloudflare_zoneid"]
+        self.waf_cloudflare_apikey = data["waf_cloudflare_apikey"]
+        self.session = None
 
-        self.vadesecure_o365_tenant = data["vadesecure_o365_tenant"]
-        self.vadesecure_o365_client_id = data["vadesecure_o365_client_id"]
-        self.vadesecure_o365_client_secret = data["vadesecure_o365_client_secret"]
+    def __connect(self):
 
-        self.access_token = data.get("vadesecure_o365_access_token", None)
-        self.expires_on = data.get("vadesecure_o365_access_token_expiry", None)
-
-
-    def __connect(self, timeout=10):
-        logger.info(f"[{__parser__}]:__connect: getting access token from {self.TOKEN_ENDPOINT}", extra={'frontend': str(self.frontend)})
-        logger.debug(f"[{__parser__}]:__connect: url -> {self.TOKEN_ENDPOINT}, client_id -> {self.vadesecure_o365_client_id}, client_secret -> [masked], timeout -> {timeout}, proxies -> {self.proxies}", extra={'frontend': str(self.frontend)})
-
-        response = requests.post(
-            self.TOKEN_ENDPOINT,
-            data={
-                'grant_type': "client_credentials",
-                'scope': "m365.api.read",
-                'client_id': self.vadesecure_o365_client_id,
-                'client_secret': self.vadesecure_o365_client_secret
-            },
-            timeout=timeout,
-            proxies=self.proxies
-        )
-
-        if response.status_code != 200:
-            if self.frontend:
-                self.frontend.vadesecure_o365_access_token = None
-                self.frontend.vadesecure_o365_access_token_expiry = None
-                self.frontend.save()
-            raise VadesecureO365APIError(f"Error on URL: {self.TOKEN_ENDPOINT} Status: {response.status_code} Reason/Content: {response.content}")
-
-        data = response.json()
-
-        self.access_token = data['access_token']
-        # decrease value by 10 seconds to ensure token is refreshed before being invalid (while still using it for most of its validity time)
-        self.expires_on = datetime.fromtimestamp(time.time() + int(data['expires_in']) - 10).replace(tzinfo=timezone.utc)
-
-        if self.frontend:
-            self.frontend.vadesecure_o365_access_token = self.access_token
-            self.frontend.vadesecure_o365_access_token_expiry = self.expires_on
-            self.frontend.save()
-
-        logger.info(f"[{__parser__}]:__connect: Successfuly got a new token, valid until {self.expires_on}", extra={'frontend': str(self.frontend)})
+        if self.session is None:
+            self.session = requests.Session()
+            self.session.headers.update(self.HEADERS)
+            self.session.headers.update({"Authorization": f"Bearer {self.waf_cloudflare_apikey}"})
 
 
-    def _get_access_token(self):
-        if not self.expires_on or not self.access_token or datetime.now(timezone.utc) > self.expires_on:
-            logger.debug(f"[{__parser__}]:_get_access_token: existing access token expired, need to get a new one", extra={'frontend': str(self.frontend)})
-            self.__connect()
-        else:
-            logger.debug(f"[{__parser__}]:_get_access_token: existing access token valid until {self.expires_on}, no need to get a new one", extra={'frontend': str(self.frontend)})
-            return self.access_token
 
-        return self._get_access_token()
+    def get_logs(self, logs_from=None, logs_to=None, test=False):
+        self.__connect()
 
+        url = f"https://api.cloudflare.com/client/v4/zones/{self.waf_cloudflare_zoneid}/logs/received"
+        query = {}
+        if logs_from:
+            query.update({'start': logs_from.strftime('%Y-%m-%dT%H:%M:%SZ')})
+        if logs_to:
+            query.update({'end': logs_to.strftime('%Y-%m-%dT%H:%M:%SZ')})
 
-    def __execute_query(self, method, url, query, timeout=10):
-        """
-        send a query to the Vadesecure 0365 endpoint, using the access_token previously recovered
-        """
-        headers = self.HEADERS
-        headers.update({"Authorization": f"Bearer {self._get_access_token()}"})
+        query.update({"fields":"BotScore,BotScoreSrc,BotTags,ClientDeviceType,ClientIP,ClientIPClass,ClientRequestBytes,ClientRequestHost,ClientRequestMethod,ClientRequestPath,ClientRequestProtocol,ClientRequestReferer,ClientRequestScheme,ClientRequestSource,ClientRequestURI,ClientRequestAgent,ClientSSIProtocol,ClientSrcPort,ClientXRequestedWith,Cookies,FirewallMatchesActions,FirewallMatchesSources,OriginIP,OriginResponseBytes,OriginResponseStatus,OriginSSLProtocol,RequestHeaders,ResponseHeaders,SecurityLevel,WAFAction,WAFRuleID,WAFRuleMessage,ZoneName"})
 
-        logger.debug(f"[{__parser__}]:__execute_query: sending request to {url}", extra={'frontend': str(self.frontend)})
-
-        if method == "POST":
-            response = requests.post(
-                url,
-                json=query,
-                headers=headers,
-                timeout=timeout,
-                proxies=self.proxies
-            )
-        else:
-            raise VadesecureO365APIError(f"Error at Vadesecure request, unknown method : {method}")
-
-        if response.status_code != 200:
-            if self.frontend:
-                self.frontend.vadesecure_o365_access_token = None
-                self.frontend.vadesecure_o365_access_token_expiry = None
-                self.frontend.save()
-            logger.info(f"[{__parser__}]:__execute_query: Error while getting logs, invalidated cached access token for next try", extra={'frontend': str(self.frontend)})
-            raise VadesecureO365APIError(f"Error at Vadesecure API Call URL: {url} Code: {response.status_code} Reason/Content: {response.content}")
-
-        return response.json()
+        logger.debug(f"{[__parser__]}:get_logs: params for request are {query}", extra={'frontend': str(self.frontend)})
 
 
-    def _get_emails_logs(self, start_from=None, limit=10):
-        url = f"{self.vadesecure_o365_host}/api/v1/tenants/{self.vadesecure_o365_tenant}/logs/emails/search"
-        query = {"sort": {"date": "asc"}, "limit": limit}
+        response = self.session.get(url, params=query)
+        response.raise_for_status()
+        content = response.text.split("\n")[:-1]
 
-        if start_from:
-            # Search_after pairs with the 'sort' parameter to return only elements corresponding to the sorted field after the element given
-            # It's the equivalent of a 'from' (excluded) condition for the date
-            # If search_after is not set, the API will return first instances of logs in database
-            query.update({"search_after": start_from})
-
-        logger.info(f"[{__parser__}]:_get_emails_logs: running query on host {self.vadesecure_o365_host}, with tenant {self.vadesecure_o365_tenant} and arguments {query}", extra={'frontend': str(self.frontend)})
-        results = self.__execute_query("POST", url, query)
-
-        try:
-            return results["result"]["messages"]
-        except KeyError:
-            logger.error(f"[{__parser__}]:_get_emails_logs: Could not get results from logs", extra={'frontend': str(self.frontend)})
-            logger.debug(f"[{__parser__}]:_get_emails_logs: got {results}", extra={'frontend': str(self.frontend)})
-            raise VadesecureO365APIError(f"Error while fetching logs from {url}: could not get messages from result")
-
-
-    def _format_log(self, log):
-        if "date" in log:
-            # parse datetime using django utils to strictly format the output (variable precision in sub-second field)
-            parsed_date = dateparse.parse_datetime(log['date'])
-            if parsed_date:
-                log["date"] = parsed_date.isoformat().replace("+00:00", "Z")
-        return log
+        logger.info(f"{[__parser__]}:get_logs: got {len(content)} new lines", extra={'frontend': str(self.frontend)})
+        return content
 
 
     def execute(self):
-        """
-            Central function to fetch logs
-        """
-        start_from = self.last_api_call.isoformat().replace("+00:00", "Z")
-        more_logs = 1
+        # Aware UTC datetime
+        current_time = datetime.now(timezone.utc)
+        since = self.last_api_call
+        if (self.last_api_call is None) or (datetime.now(timezone.utc) - self.last_api_call >= timedelta(seconds=62)):
+            since = (datetime.now(timezone.utc) - timedelta(seconds=62))
+        #The API has a delay of 60 seconds
+        to = current_time - timedelta(seconds=60)
+        logs = self.get_logs(logs_from=since, logs_to=to)
+        self.update_lock()
+        # increment by 1ms to avoid repeating a line if its timestamp happens to be the exact timestamp 'to'
+        self.last_api_call = to + timedelta(microseconds=1000)
+        self.write_to_file(logs)
 
-        while more_logs:
-            data = []
-            logs = self._get_emails_logs(start_from, limit=200)
-
-            if logs:
-                more_logs = 1
-                logger.info(f"[{__parser__}]:execute: got {len(logs)} logs", extra={'frontend': str(self.frontend)})
-                for log in logs:
-                    data.append(json.dumps(self._format_log(log)))
-
-                try:
-                    start_from = logs[-1]['date']
-                except KeyError:
-                    logger.error(f"[{__parser__}]:execute: Could not get 'date' key from last log", extra={'frontend': str(self.frontend)})
-                    logger.debug(f"[{__parser__}]:execute: log line is '{logs[-1]}'", extra={'frontend': str(self.frontend)})
-                    raise VadesecureO365APIError(f"Could not get date from log")
-
-                # Writting may take a while, so refresh token in Redis
-                self.update_lock()
-                self.write_to_file(data)
-            else:
-                logger.info(f"[{__parser__}]:execute: finished processing logs", extra={'frontend': str(self.frontend)})
-                more_logs = 0
-
-        self.frontend.last_api_call = dateparse.parse_datetime(start_from)
-        self.frontend.save()
-
-        logger.info(f"[{__parser__}]:execute: Parsing done.", extra={'frontend': str(self.frontend)})
+        logger.info(f"{[__parser__]}:execute: Parsing done.", extra={'frontend': str(self.frontend)})
 
 
     def test(self):
-        logger.info(f"[{__parser__}]:test: Launching test", extra={'frontend': str(self.frontend)})
-
+        current_time = datetime.now(timezone.utc)
         try:
-            # Invalidate potential access_token to force a refresh with updated parameters
-            self.expires_on = None
-            result = self._get_emails_logs()
+            logs = self.get_logs(logs_from=(current_time - timedelta(seconds=62)), logs_to=(current_time - timedelta(seconds=61)))
 
             return {
                 "status": True,
-                "data": result
+                "data": logs
             }
         except Exception as e:
-            logger.exception(f"[{__parser__}]:test: {e}", extra={'frontend': str(self.frontend)})
+            logger.exception(f"{[__parser__]}:test: {str(e)}", extra={'frontend': str(self.frontend)})
             return {
                 "status": False,
                 "error": str(e)
