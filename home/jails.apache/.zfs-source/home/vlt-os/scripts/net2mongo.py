VultureProject/vulture-gui

commit c8359716eb66c7fc5ebefc35fce80001700267d2

NET2MONGO::FIXED:: While searching for dhcp options on interfaces, be more permissive on syntaxes

home/jails.apache/.zfs-source/home/vlt-os/scripts/net2mongo.py
@@ -63,7 +63,7 @@ if __name__ == "__main__":
 
     """ Read system configuration """
     with open("/etc/rc.conf.d/network", "r") as f:
-        pattern_ifconfig = re.compile("^ifconfig_(.*)=(.*)")
+        pattern_ifconfig = re.compile('^ifconfig_(.*)="?(.*)"?')
         pattern_inet6 = re.compile("inet6 (.*)(( prefixlen )|(/))([0-9\.]+)")
         pattern_inet = re.compile("inet (.*)(( netmask )|(/))([0-9\.]+)")
         pattern_gateway = re.compile("^defaultrouter=(.*)")
@@ -113,7 +113,7 @@ if __name__ == "__main__":
                     logger.debug("Node::network_sync(): Detected NIC {}".format(nic))
 
 
-            if config in ("SYNCDHCP", "DHCP"):
+            if config.upper() in ("SYNCDHCP", "DHCP"):
                 try:
                     proc = subprocess.Popen([
                         '/usr/local/bin/sudo',

commit cfff2fdd3ad1ff901a74e9fb88095081493b67db

NET2MONGO::FIXED:: Resolve script failure caused by c8359716

home/jails.apache/.zfs-source/home/vlt-os/scripts/net2mongo.py
@@ -113,7 +113,7 @@ if __name__ == "__main__":
                     logger.debug("Node::network_sync(): Detected NIC {}".format(nic))
 
 
-            if config.upper() in ("SYNCDHCP", "DHCP"):
+            if config and config.upper() in ("SYNCDHCP", "DHCP"):
                 try:
                     proc = subprocess.Popen([
                         '/usr/local/bin/sudo',