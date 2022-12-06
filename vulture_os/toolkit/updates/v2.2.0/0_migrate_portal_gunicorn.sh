499a9b18bfa4affc4de3bae2f3c58211a193f7d8
Fabien Amelinck <fabien.amelinck@advens.fr>
2022-12-06 15:59:54 +0100

    GUI::Mounting sockets folder in apache jail

--- vulture_os/toolkit/updates/v2.2.0/0_migrate_portal_gunicorn.sh
+++ vulture_os/toolkit/updates/v2.2.0/0_migrate_portal_gunicorn.sh
@@ -10,9 +10,27 @@ echo "[+] Installation of nginx into apache jail"
 /usr/sbin/pkg -j apache install -y nginx || (/bin/echo "Fail to install nginx!" ; exit 1)
 echo "[*] done"
 
+echo "[+] Creation of the gui socket folder"
+/bin/mkdir -p /var/sockets/gui/
+chmod 770 /var/sockets/gui
+chown root:vlt-web /var/sockets/gui/
+
 /bin/mkdir -p /zroot/apache/var/sockets/gui/
 chmod 770 /zroot/apache/var/sockets/gui
 chown root:vlt-web /zroot/apache/var/sockets/gui
+echo "[*] done"
 
+echo "[+] Mounting host folder in apache jail"
+mount_path="/var/sockets/gui /zroot/apache/var/sockets/gui"
+if [ "$(/usr/bin/grep "$mount_path" "/etc/fstab" 2> /dev/null)" == "" ]  ; then
+    /bin/echo "$mount_path nullfs   rw,late      0       0" >> "/etc/fstab"
+fi
+if [ "$(/sbin/mount -p | /usr/bin/sed -E 's/[[:cntrl:]]+/ /g' | /usr/bin/grep "$mount_path")" == "" ] ; then
+    /sbin/mount_nullfs -o rw,late $mount_path
+fi
+echo "[*] done"
+
+echo "[+] Enabling gunicorn and nginx services"
 /bin/echo 'gunicorn_enable="YES"' > /zroot/apache/etc/rc.conf.d/gunicorn
 /bin/echo 'nginx_enable="YES"' > /zroot/apache/etc/rc.conf.d/nginx
+echo "[*] done"
