94a5eab4680cc92a3d7df608057772e72432f418
Fabien AMELINCK <49721371+ELDiablO59152@users.noreply.github.com>
2022-12-06 16:30:10 +0100

    GUI::Transition from 'apache' to 'gunicorn' (#265)
    ### Added
    - [GUI] [SYSTEM] Add nginx in the apache jail to work as proxy and serve static files for gunicorn
    ### Changed
    - [GUI] [SYSTEM] Replace apache's WSGI with gunicorn to serve GUI
    ### Removed
    - [APACHE] Completely remove apache package and logic in code (service, handling, etc...)

---
+++ vulture_os/toolkit/updates/v2.2.0/0_migrate_portal_gunicorn.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+echo "[+] Stopping apache into apache jail"
+/usr/sbin/jexec apache /usr/sbin/service apache24 stop
+echo "[*] done"
+
+/bin/rm /zroot/apache/etc/rc.conf.d/apache24
+
+echo "[+] Installation of nginx into apache jail"
+/usr/sbin/pkg -j apache remove -y www/apache24 || (/bin/echo "apache24 already removed!" ; exit 1)
+/usr/sbin/pkg -j apache remove -y www/mod_wsgi4 || (/bin/echo "mod_wsgi4 already removed!" ; exit 1)
+/usr/sbin/pkg -j apache autoremove -y
+# Ensure python is installed (can be removed through mod_wsgi4)
+/usr/sbin/pkg -j apache install -y lang/python || (/bin/echo "Fail to install python!" ; exit 1)
+/usr/sbin/pkg -j apache install -y nginx || (/bin/echo "Fail to install nginx!" ; exit 1)
+echo "[*] done"
+
+echo "[+] Creation of the gui socket folder"
+/bin/mkdir -p /var/sockets/gui/
+chmod 770 /var/sockets/gui
+chown root:vlt-web /var/sockets/gui/
+
+/bin/mkdir -p /zroot/apache/var/sockets/gui/
+chmod 770 /zroot/apache/var/sockets/gui
+chown root:vlt-web /zroot/apache/var/sockets/gui
+echo "[*] done"
+
+echo "[+] Mounting host folder in apache jail"
+mount_path="/var/sockets/gui /zroot/apache/var/sockets/gui"
+if [ "$(/usr/bin/grep "$mount_path" "/etc/fstab" 2> /dev/null)" == "" ]  ; then
+    /bin/echo "$mount_path nullfs   rw,late      0       0" >> "/etc/fstab"
+fi
+if [ "$(/sbin/mount -p | /usr/bin/sed -E 's/[[:cntrl:]]+/ /g' | /usr/bin/grep "$mount_path")" == "" ] ; then
+    /sbin/mount_nullfs -o rw,late $mount_path
+fi
+echo "[*] done"
+
+echo "[+] Enabling gunicorn and nginx services"
+/bin/echo 'gunicorn_enable="YES"' > /zroot/apache/etc/rc.conf.d/gunicorn
+/bin/echo 'nginx_enable="YES"' > /zroot/apache/etc/rc.conf.d/nginx
+echo "[*] done"
