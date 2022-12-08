05b83d62fedfac51f0e9007b21c9a1629e2753c7
Théo Bertin <theo.bertin@advens.fr>
2022-12-08 16:00:16 +0100

    UPDATE_SCRIPT::CHANGED:: remove useless folders creation in 2.2.0 script (done by POST_INSTALL)

--- vulture_os/toolkit/updates/v2.2.0/0_migrate_gui_gunicorn.sh
+++ vulture_os/toolkit/updates/v2.2.0/0_migrate_gui_gunicorn.sh
@@ -15,17 +15,7 @@ echo "[+] Installation of nginx into apache jail"
 /usr/sbin/pkg -j apache install -y nginx || (/bin/echo "Fail to install nginx!" ; exit 1)
 echo "[*] done"
 
-echo "[+] Creation of the gui socket folder"
-/bin/mkdir -p /var/sockets/gui/
-chmod 770 /var/sockets/gui
-chown root:vlt-web /var/sockets/gui/
-
-/bin/mkdir -p /zroot/apache/var/sockets/gui/
-chmod 770 /zroot/apache/var/sockets/gui
-chown root:vlt-web /zroot/apache/var/sockets/gui
-echo "[*] done"
-
-echo "[+] Mounting host folder in apache jail"
+echo "[+] Mounting new folders in apache jail"
 mount_path="/var/sockets/gui /zroot/apache/var/sockets/gui"
 if [ "$(/usr/bin/grep "$mount_path" "/etc/fstab" 2> /dev/null)" == "" ]  ; then
     /bin/echo "$mount_path nullfs   rw,late      0       0" >> "/etc/fstab"
