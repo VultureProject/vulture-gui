4ab1b19d1fc1877a671a0b4f11f88c2d97169dfd
Théo Bertin <theo.bertin@advens.fr>
2023-02-23 12:44:24 +0100

    PORTAL::CHANGED:: Return a 401 instead of a 403 when refusing a header-authenticated request

---
+++ vulture_os/toolkit/updates/next_release/2_reload_authenticated_frontends.py
@@ -0,0 +1,61 @@
+#!/home/vlt-os/env/bin/python
+
+"""This file is part of Vulture 4.
+
+Vulture 4 is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Vulture 4 is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Vulture 4.  If not, see http://www.gnu.org/licenses/.
+"""
+__author__ = "Theo Bertin"
+__credits__ = []
+__license__ = "GPLv3"
+__version__ = "4.0.0"
+__maintainer__ = "Vulture Project"
+__email__ = "contact@vultureproject.org"
+__doc__ = "Reload Haproxy portal frontends configuration"
+
+import sys
+import os
+
+# Django setup part
+sys.path.append('/home/vlt-os/vulture_os')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", 'vulture_os.settings')
+
+import django
+from django.conf import settings
+django.setup()
+
+from system.cluster.models import Cluster, Node
+from services.frontend.models import Frontend
+
+if not Cluster.is_node_bootstrapped():
+    sys.exit(0)
+
+if __name__ == "__main__":
+
+    node = Cluster.get_current_node()
+    if not node:
+        print("Current node not found. Maybe the cluster has not been initialised yet.")
+    else:
+        try:
+            # Reload all related frontends' Haproxy configuration file
+            for frontend in Frontend.objects.filter(workflow__authentication__isnull=False).distinct():
+                node.api_request("services.haproxy.haproxy.build_conf", frontend.id)
+                print("Frontend '{}' conf reload asked".format(frontend))
+
+            node.api_request("services.haproxy.haproxy.reload_service")
+
+        except Exception as e:
+            print("Failed to update authentication related frontend configurations: {}".format(e))
+            print("Please relaunch this script after solving the issue.")
+
+        print("Done.")
