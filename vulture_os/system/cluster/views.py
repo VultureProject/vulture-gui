c45ecc0b87e38baef31476b3852673c86b115b32
Théo Bertin <theo.bertin@advens.fr>
2023-07-17 18:17:48 +0200

    NODE::FIXED:: Correctly update rc.conf when editing a Node on GUI/API

--- vulture_os/system/cluster/views.py
+++ vulture_os/system/cluster/views.py
@@ -174,10 +174,26 @@ def cluster_edit(request, object_id, api=False, update=False):
         if ip_changed:
             Cluster.api_request("toolkit.network.network.make_hostname_resolvable", (node.name, node.management_ip))
             RC_FILENAME = "network"
-            node.api_request('toolkit.system.rc.call_set_rc_config', (RC_FILENAME, "management_ip", node.management_ip))
-            node.api_request('toolkit.system.rc.call_set_rc_config', (RC_FILENAME, "internet_ip", node.internet_ip))
-            node.api_request('toolkit.system.rc.call_set_rc_config', (RC_FILENAME, "backends_outgoing_ip", node.backends_outgoing_ip))
-            node.api_request('toolkit.system.rc.call_set_rc_config', (RC_FILENAME, "logom_outgoing_ip", node.logom_outgoing_ip))
+            node.api_request('toolkit.system.rc.call_set_rc_config', {
+                "variable": "management_ip",
+                "value": node.management_ip,
+                "filename": RC_FILENAME,
+                })
+            node.api_request('toolkit.system.rc.call_set_rc_config', {
+                "variable": "internet_ip",
+                "value": node.internet_ip,
+                "filename": RC_FILENAME,
+                })
+            node.api_request('toolkit.system.rc.call_set_rc_config', {
+                "variable": "backends_outgoing_ip",
+                "value": node.backends_outgoing_ip,
+                "filename": RC_FILENAME,
+                })
+            node.api_request('toolkit.system.rc.call_set_rc_config', {
+                "variable": "logom_outgoing_ip",
+                "value": node.logom_outgoing_ip,
+                "filename": RC_FILENAME,
+                })
 
         if pstats_forwarders_changed:
             node.api_request("services.rsyslogd.rsyslog.configure_pstats")
