d9a64c90bc7848825e8b15bfbc0aca13baf6f091
Théo Bertin <theo.bertin@advens.fr>
2023-09-20 11:57:11 +0200

    FRONTEND::CHANGED:: Set new default redis server to '127.0.0.5'
    This points to the load-balanced Haproxy flow, allowing for automatic and transparent redirection to the current redis master node

---
+++ vulture_os/toolkit/updates/v2.10.2/0_change_frontend_redis_server_default.py
@@ -0,0 +1,59 @@
+#!/home/vlt-os/env/bin/python
+
+"""This file is part of Vulture 4.
+
+Vulture 4 is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Vulture 4 is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Vulture 4.  If not, see http://www.gnu.org/licenses/.
+"""
+__author__ = "Theo Bertin"
+__credits__ = []
+__license__ = "GPLv3"
+__version__ = "4.0.0"
+__maintainer__ = "Vulture Project"
+__email__ = "contact@vultureproject.org"
+__doc__ = "Reload Haproxy portals configuration"
+
+import sys
+import os
+
+# Django setup part
+sys.path.append('/home/vlt-os/vulture_os')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", 'vulture_os.settings')
+
+import django
+from django.conf import settings
+django.setup()
+
+from system.cluster.models import Cluster
+from services.frontend.models import Frontend
+
+if not Cluster.is_node_bootstrapped():
+    sys.exit(0)
+
+if __name__ == "__main__":
+
+    node = Cluster.get_current_node()
+    if not node:
+        print("Current node not found. Maybe the cluster has not been initialised yet.")
+    else:
+        try:
+            for frontend in Frontend.objects.filter(redis_server="127.0.0.3"):
+                print(f"Updating frontend {frontend.name}'s default redis server from '127.0.0.3' to '127.0.0.5'")
+                frontend.redis_server = "127.0.0.5"
+                frontend.save()
+
+        except Exception as e:
+            print("Failed to update Frontends configuration: {}".format(e))
+            print("Please relaunch this script after solving the issue.")
+
+        print("Done.")
