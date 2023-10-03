1bae5834616434a95c4e99ed71aef27bbbbfb45b
Théo Bertin <theo.bertin@advens.fr>
2023-10-03 12:07:44 +0200

    UPDATE_SCRIPTS::FIXED::v2.10.2 Avoid updating Frontend's Redis internal IP when a custom port is used

--- vulture_os/toolkit/updates/v2.10.2/0_change_frontend_redis_server_default.py
+++ vulture_os/toolkit/updates/v2.10.2/0_change_frontend_redis_server_default.py
@@ -47,7 +47,7 @@ if __name__ == "__main__":
         print("Current node not found. Maybe the cluster has not been initialised yet.")
     else:
         try:
-            for frontend in Frontend.objects.filter(redis_server="127.0.0.3"):
+            for frontend in Frontend.objects.filter(redis_server="127.0.0.3", redis_port=6379):
                 print(f"Updating frontend {frontend.name}'s default redis server from '127.0.0.3' to '127.0.0.5'")
                 frontend.redis_server = "127.0.0.5"
                 frontend.save()
