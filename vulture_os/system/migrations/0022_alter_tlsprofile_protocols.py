2c8e57401c5b2ae0c1e867e9d024faa893ccbd99
Théo Bertin <theo.bertin@advens.fr>
2023-10-13 15:00:01 +0200

    MIGRATIONS::FIXED:: Correct tlsprofile profiles' migration

--- vulture_os/system/migrations/0022_alter_tlsprofile_protocols.py
+++ vulture_os/system/migrations/0022_alter_tlsprofile_protocols.py
@@ -4,6 +4,33 @@ from django.db import migrations, models
 import djongo.models.fields
 
 
+existing_profiles_protocols = {}
+
+def save_tls_protocols(apps, schema_editor):
+    tls_profile_model = apps.get_model("system", "tlsprofile")
+    db_alias = schema_editor.connection.alias
+    tls_profile = tls_profile_model.objects.using(db_alias)
+
+    for profile in tls_profile.all():
+        existing_profiles_protocols[profile.id] = profile.protocols
+
+
+def update_tls_protocols(apps, schema_editor):
+    tls_profile_model = apps.get_model("system", "tlsprofile")
+    db_alias = schema_editor.connection.alias
+    tls_profile = tls_profile_model.objects.using(db_alias)
+
+    for profile in tls_profile.all():
+        # put back configured protocols
+        profile.protocols = existing_profiles_protocols[profile.id]
+
+        # replace old default with new default
+        if profile.protocols == ['tlsv12']:
+            profile.protocols = ['tlsv13', 'tlsv12']
+
+        profile.save()
+
+
 class Migration(migrations.Migration):
 
     dependencies = [
@@ -11,9 +38,15 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.AlterField(
-            model_name='tlsprofile',
-            name='protocols',
+        migrations.RunPython(save_tls_protocols, migrations.RunPython.noop),
+        migrations.RemoveField(
+            model_name="tlsprofile",
+            name="protocols",
+        ),
+        migrations.AddField(
+            model_name="tlsprofile",
+            name="protocols",
             field=djongo.models.fields.JSONField(default=['tlsv13', 'tlsv12'], help_text='Allowed protocol ciphers.', verbose_name=models.TextField(choices=[('tlsv13', 'TLSv1.3'), ('tlsv12', 'TLSv1.2'), ('tlsv11', 'TLSv1.1'), ('tlsv10', 'TLSv1.0')])),
         ),
+        migrations.RunPython(update_tls_protocols, migrations.RunPython.noop),
     ]
