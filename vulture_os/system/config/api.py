9e06fa60f013bbc836202e58b563629b60e14ef0
Théo Bertin <theo.bertin@advens.fr>
2023-10-24 11:49:49 +0200

    CLUSTER_CONFIG_API::FIXED:: add missing allowed_fields for PATCH operation + minor fixes

--- vulture_os/system/config/api.py
+++ vulture_os/system/config/api.py
@@ -78,16 +78,18 @@ class ConfigAPIv1(View):
         }, status=500)
 
     @api_need_key('cluster_api_key')
-    def patch(self, request):
+    def patch(self, request, object_id):
         allowed_fields = ('pf_ssh_restrict', 'pf_admin_restrict', 'pf_whitelist', 'pf_blacklist',
                           'cluster_api_key', 'ldap_repository', 'oauth2_header_name', 'portal_cookie_name',
-                          'public_token', 'branch', 'smtp_server', 'ssh_authorized_key', 'rsa_encryption_key')
+                          'public_token', 'branch', 'smtp_server', 'ssh_authorized_key', 'rsa_encryption_key',
+                          'logs_ttl', 'internal_tenants')
         try:
             for key in request.JSON.keys():
                 if key not in allowed_fields:
                     return JsonResponse({'error': _("Attribute not allowed : '{}'."
-                                                    "Allowed attributes are {}".format(key, allowed_fields))})
-            return config_edit(request, api=True, update=True)
+                                                    "Allowed attributes are {}".format(key, allowed_fields))},
+                                                    status=400)
+            return config_edit(request, object_id, api=True, update=True)
 
         except Exception as e:
             logger.critical(e, exc_info=1)
