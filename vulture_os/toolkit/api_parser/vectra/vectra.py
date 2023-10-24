d4b37220d55095f0b38b765b2467d634bfc008e9
Théo Bertin <theo.bertin@advens.fr>
2023-10-24 12:18:01 +0200

    [API_PARSER][VECTRA]::FIXED:: Heavy fixes and corrections

--- vulture_os/toolkit/api_parser/vectra/vectra.py
+++ vulture_os/toolkit/api_parser/vectra/vectra.py
@@ -26,7 +26,8 @@ __parser__ = 'VECTRA'
 import logging
 import requests
 import json
-from datetime import datetime, timedelta
+from copy import deepcopy
+from datetime import timedelta, datetime
 from django.utils import timezone
 from django.conf import settings
 from toolkit.api_parser.api_parser import ApiParser
@@ -53,14 +54,13 @@ class VectraParser(ApiParser):
         self.vectra_secret_key = data["vectra_secret_key"]
 
         self.access_token = None
-        self.expires_in = None
+        self.expire_at = None
         self.session = None
 
-    def auth_token(self):
-
+    def get_auth_token(self):
         res = {}
 
-        logger.info("Generating access token.")
+        logger.info(f"[{__parser__}]:get_auth_token: Generating access token.", extra={'frontend': str(self.frontend)})
         url = self.vectra_host + self.TOKEN_ENDPOINT
         try:
             res = requests.post(
@@ -77,12 +77,16 @@ class VectraParser(ApiParser):
             if res.status_code == 429:
                 raise VectraAPIError("Too many requests.")
             res.raise_for_status()
-            logger.info("Access token is generated.")
+            logger.info(f"[{__parser__}]:get_auth_token: Access token generated.", extra={'frontend': str(self.frontend)})
 
             return res.json().get("access_token"), res.json().get('expires_in')
-            
+
         except Exception as e:
-            logger.error(f"An exception occurred: {e} response: {res.status_code}")
+            logger.error(f"[{__parser__}]:get_auth_token: An exception occurred: {e}", extra={'frontend': str(self.frontend)})
+            if res:
+                logger.error(f"[{__parser__}]:get_auth_token: response: {res.status_code}", extra={'frontend': str(self.frontend)})
+            raise e
+
 
     def _connect(self):
         try:
@@ -90,77 +94,61 @@ class VectraParser(ApiParser):
                 self.session = requests.Session()
 
                 headers = self.HEADERS
-                
+
                 self.session.headers.update(headers)
+
+            # Check for expiration with 10 seconds difference to be sure token will still be valid for some time
+            if not self.access_token or not self.expire_at or (self.expire_at - timedelta(seconds=10)) < timezone.now():
+                access_token, expires_in = self.get_auth_token()
+                self.access_token = access_token
+                self.expire_at = timezone.now() + timedelta(seconds=expires_in)
                 self.session.headers.update({"Authorization": f"Bearer {self.access_token}"})
-
-            return True
-
         except Exception as err:
-            raise VectraAPIError
+            raise VectraAPIError(err)
+
+        return True
 
 
     def get_data_from_entity_api(self, query = {}):
-
-        url = f"{self.vectra_host.strip().strip('/')}/api/v3.3/events/entity_scoring"
+        results = list()
+        # Ensure initial query object is not modified by the function
+        internal_query = deepcopy(query)
         types = [
             "account",
             "host"
         ]
-        for typ in types:
-            logger.info(f"Executing Entity {typ} API task.")
-            query.update({"type": typ})
-            response = self.session.get(
-                url,
-                params=query,
-                timeout=20,
-                proxies=self.proxies,
-                verify=self.api_parser_custom_certificate if self.api_parser_custom_certificate else self.api_parser_verify_ssl
-            )
+        for type_value in types:
+            logger.info(f"[{__parser__}]:get_data_from_entity_api: Executing Entity {type_value} API task.", extra={'frontend': str(self.frontend)})
+            internal_query.update({"type": type_value})
+            response = self.__execute_query(internal_query, "api/v3.3/events/entity_scoring")
 
-            # Other error
-            if response.status_code != 200:
-                raise VectraAPIError(
-                    f"Error at Vectra API Call URL: {url} Code: {response.status_code} Content: {response.content}, Headers: {self.session.headers}, Query: f{query}")
-            
-            results = response.json()["events"]
-            if response.json()["next_checkpoint"]:
-                query.update({"from": response.json()["next_checkpoint"]})
-                response = self.session.get(
-                    url,
-                    params=query,
-                    timeout=20,
-                    proxies=self.proxies,
-                    verify=self.api_parser_custom_certificate if self.api_parser_custom_certificate else self.api_parser_verify_ssl
-                )
-                results += response.json()["events"]
+            results.extend(response.get("events", []))
+            while "remaining_count" in response and "next_checkpoint" in response and response["remaining_count"] != 0:
+                internal_query.update({"from": response["next_checkpoint"]})
+                response = self.__execute_query(internal_query, "/api/v3.3/events/entity_scoring")
+                results.extend(response.get("events", []))
+
         return results
 
-        
-    def get_logs(self, since=None, to=None):
 
-        if self.expires_in is None or self.expires_in < timezone.now():
-            access_token, expires_in = self.auth_token()
-            self.access_token = access_token
-            self.expires_in = timezone.now() + timedelta(seconds=expires_in)
-
+    def get_logs(self, since: datetime, to: datetime):
+        results = list()
         query = {"event_timestamp_gte": since.strftime("%Y-%m-%dT%H:%M:%SZ"), "event_timestamp_lte": to.strftime("%Y-%m-%dT%H:%M:%SZ")}
-        results = []
         for endpoint in ["api/v3.3/events/detections"]:
             response = self.__execute_query(query, endpoint)
-            results += response["events"]
-        results += self.get_data_from_entity_api(query)
-        logger.info(f"results: {results}")
+            results.extend(response.get("events", []))
+        results.extend(self.get_data_from_entity_api(query))
         return results
+
 
     def __execute_query(self, query, endpoint, timeout=20):
 
         self._connect()
 
-        url = self.vectra_host + endpoint
+        url = self.vectra_host.strip('/') + '/' + endpoint.strip('/')
 
         msg = "URL : " + url + " Query : " + str(query)
-        logger.info(f"[{__parser__}] Request API : {msg}", extra={'frontend': str(self.frontend)})
+        logger.info(f"[{__parser__}]:__execute_query: Request API : {msg}", extra={'frontend': str(self.frontend)})
 
         response = self.session.get(
             url,
@@ -174,18 +162,19 @@ class VectraParser(ApiParser):
         if response.status_code != 200:
             raise VectraAPIError(
                 f"Error at Vectra API Call URL: {url} Code: {response.status_code} Content: {response.content}, Headers: {self.session.headers}, Query: f{query}")
-        
+
         return response.json()
-    
-    def format_log(log):
+
+
+    def format_log(self, log):
         return json.dumps(log)
 
-     
+
     def test(self):
         logger.info(f"[{__parser__}]:test: Launching test", extra={'frontend': str(self.frontend)})
 
         try:
-            self.expires_in = None
+            self.expire_at = None
             since = timezone.now() - timedelta(hours=24)
             to = timezone.now()
             result = self.get_logs(since, to)
@@ -195,29 +184,29 @@ class VectraParser(ApiParser):
                 "data": result
             }
         except Exception as e:
-            logger.exception(f"[{__parser__}]:test: {e}", extra={'frontend': str(self.frontend)})
+            logger.error(f"[{__parser__}]:test: {e}", extra={'frontend': str(self.frontend)})
             return {
                 "status": False,
                 "error": str(e)
             }
+
 
     def execute(self):
         try:
             ## GET LOGS ##
             since = self.frontend.last_api_call or (timezone.now() - timedelta(days=1))
             to = min(timezone.now(), since + timedelta(hours=24))
-            
-            logger.info(f"[{__parser__}]: get_logs since {since} to {to}", extra={'frontend': str(self.frontend)})
+
+            logger.info(f"[{__parser__}]:execute: getting logs from {since} to {to}", extra={'frontend': str(self.frontend)})
             results = self.get_logs(since, to)
-            
             self.update_lock()
-            self.frontend.last_api_call = to
-            
+
             # Send those lines to Rsyslog
-            logger.info(f"Logs: {results}")
             self.write_to_file([self.format_log(l) for l in results])
+
             # And update lock after sending lines to Rsyslog
             self.update_lock()
+            self.frontend.last_api_call = to
             self.frontend.save()
         except Exception as e:
             logger.exception(f"[{__parser__}]:execute: {e}", extra={'frontend': str(self.frontend)})
