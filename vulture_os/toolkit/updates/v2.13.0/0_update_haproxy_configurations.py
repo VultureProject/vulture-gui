7d85c0be4f8d23805a59dc40a1871ecf2b073b4b
Théo Bertin <theo.bertin@advens.fr>
2023-11-15 16:13:40 +0100

    UPDATE_SCRIPTS::ADDED:: New 2.13.0 script to reload Haproxy configurations

---
+++ vulture_os/toolkit/updates/v2.13.0/0_update_haproxy_configurations.py
@@ -0,0 +1,68 @@
+#!/home/vlt-os/env/bin/python
+
+"""This file is part of Vulture 4.
+
+Vulture 4 is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Vulture 4 is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Vulture 4.  If not, see http://www.gnu.org/licenses/.
+"""
+__author__ = "Theo Bertin"
+__credits__ = []
+__license__ = "GPLv3"
+__version__ = "4.0.0"
+__maintainer__ = "Vulture Project"
+__email__ = "contact@vultureproject.org"
+__doc__ = "Reload the default TLSProfile object and underlying configurations"
+
+import sys
+import os
+
+# Django setup part
+sys.path.append('/home/vlt-os/vulture_os')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", 'vulture_os.settings')
+
+import django
+from django.conf import settings
+django.setup()
+
+from system.cluster.models import Cluster
+from workflow.models import Workflow
+from services.frontend.models import Frontend
+
+if not Cluster.is_node_bootstrapped():
+    sys.exit(0)
+
+if __name__ == "__main__":
+
+    node = Cluster.get_current_node()
+    if not node:
+        print("Current node not found. Maybe the cluster has not been initialised yet.")
+    else:
+        try:
+            for workflow in Workflow.objects.all():
+                print(f"Rebuilding configuration for Workflow '{workflow.name}'")
+                node.api_request("workflow.workflow.build_conf", workflow.pk)
+
+            for frontend in Frontend.objects.filter(mode__in=['tcp', 'http'], workflow__isnull=False).distinct():
+                print(f"Rebuilding configuration for Frontend '{frontend.name}'")
+                node.api_request("services.haproxy.haproxy.build_conf", frontend.pk)
+
+            api_res = node.api_request("services.haproxy.haproxy.reload_service")
+            if not api_res.get('status'):
+                print("API error while trying to "
+                            "restart HAProxy service : {}".format(api_res.get('message')))
+
+        except Exception as e:
+            print("Failed to update some Haproxy configurations: {}".format(e))
+            print("Please relaunch this script after solving the issue.")
+
+        print("Done.")
