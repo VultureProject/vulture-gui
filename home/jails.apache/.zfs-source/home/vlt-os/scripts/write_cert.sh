737aa3948261da407a9ad5a53fa2ce9e2674649b
Fabien Amelinck <fabien.amelinck@advens.fr>
2024-01-11 16:50:53 +0100

    WRITE_CERT::FIXED: handle ca.key

--- home/jails.apache/.zfs-source/home/vlt-os/scripts/write_cert.sh
+++ home/jails.apache/.zfs-source/home/vlt-os/scripts/write_cert.sh
@@ -1,15 +1,18 @@
 #!/bin/sh
 
 mv -f /var/tmp/ca.pem /var/db/pki/
+mv -f /var/tmp/ca.key /var/db/pki/
 mv -f /var/tmp/node.cert /var/db/pki/
 mv -f /var/tmp/node.key /var/db/pki/
 mv -f /var/tmp/node.pem /var/db/pki/
 
 chown root:vlt-conf /var/db/pki/ca.pem
+chown root:vlt-conf /var/db/pki/ca.key
 chown root:vlt-conf /var/db/pki/node.cert
 chown root:vlt-conf /var/db/pki/node.key
 chown root:vlt-conf /var/db/pki/node.pem
 chmod 440 /var/db/pki/ca.pem
+chmod 440 /var/db/pki/ca.key
 chmod 440 /var/db/pki/node.cert
 chmod 440 /var/db/pki/node.key
 chmod 440 /var/db/pki/node.pem
