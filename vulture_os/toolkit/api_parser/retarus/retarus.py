b0852ac4285adf404143d09acb58d19d9d8833e9
Nicolas Lançon <nicolas.lancon@advens.fr>
2024-01-15 17:05:35 +0100

    RETARUS:: Fix updating lock to prevent multiprocess

--- vulture_os/toolkit/api_parser/retarus/retarus.py
+++ vulture_os/toolkit/api_parser/retarus/retarus.py
@@ -23,14 +23,15 @@ __email__ = "contact@vultureproject.org"
 __doc__ = 'RETARUS API'
 __parser__ = 'RETARUS'
 
+import datetime
 import json
 import logging
-import time
 
 import websocket
-from threading import Thread
+from urllib.parse import urlparse
 
 from django.conf import settings
+from django.utils import timezone
 from toolkit.api_parser.api_parser import ApiParser
 
 logging.config.dictConfig(settings.LOG_SETTINGS)
@@ -55,6 +56,26 @@ class RetarusParser(ApiParser):
         self.retarus_token = data["retarus_token"]
         self.retarus_channel = data["retarus_channel"]
 
+        self.buffer = list()
+
+    def _connect(self):
+        try:
+            extra_headers = {
+                'header': [f"Authorization: Bearer {self.retarus_token}"],
+            }
+            url = f"{self.ENDPOINT}?channel={self.retarus_channel}"
+
+            if self.proxies:
+                proxy_ = urlparse(self.proxies.get('http'))
+                proxy_split = proxy_.netloc.split(':')
+                extra_headers['http_proxy_host'] = proxy_split[0]
+                extra_headers['http_proxy_port'] = proxy_split[1] if len(proxy_split) > 1 else 80
+
+            return websocket.create_connection(url, timeout=20, **extra_headers)
+        except Exception as e:
+            logger.error(f"[{__parser__}]:_connect: WebSocket connection error: {e}", extra={'frontend': str(self.frontend)})
+            raise RetarusAPIError(f"Could not connect to API: {e}")
+
     def test(self):
 
         ws = websocket.create_connection(self.ENDPOINT, header=["Authorization: Bearer " + self.retarus_token])
@@ -75,43 +96,31 @@ class RetarusParser(ApiParser):
     def format_log(log):
         return json.dumps(log)
 
-    def get_logs(self):
-        url = f"{self.ENDPOINT}?channel={self.retarus_channel}"
-        ws = websocket.WebSocketApp(
-            url,
-            header=[f"Authorization: Bearer {self.retarus_token}"],
-            on_open=self.on_open,
-            on_message=self.on_message,
-            on_error=self.on_error,
-            on_close=self.on_close
-        )
-        ws.run_forever(ping_interval=60)
-
-    def _update_lock(self):
-        while not self.evt_stop.is_set():
-            # Updating lock every 60s to prevent multiprocessing
-            self.update_lock()
-            time.sleep(60)
+    def _flush_buffer(self):
+        if self.buffer:
+            self.write_to_file(self.buffer)
+            self.buffer.clear()
 
     def execute(self):
-        thread_get_logs = Thread(target=self.get_logs)
-        thread_update_lock = Thread(target=self._update_lock)
+        current_time = timezone.now()
+        ws = self._connect()
 
-        thread_get_logs.start()
-        thread_update_lock.start()
+        try:
+            while not self.evt_stop.is_set():
+                if msg := ws.recv():
+                    self.buffer.append(self.format_log(msg))
+                    if len(self.buffer) >= 128:
+                        self._flush_buffer()
 
-        thread_get_logs.join()
-        thread_update_lock.join()
+                if timezone.now() >= (current_time + datetime.timedelta(seconds=10)):
+                    current_time = timezone.now()
+                    self.update_lock()
+        except websocket._exceptions.WebSocketConnectionClosedException:
+            logger.info(f"[{__parser__}]:execute: Connection was closed, finalizing...", extra={'frontend': str(self.frontend)})
+        except Exception as e:
+            logger.error(f"[{__parser__}]:execute: WebSocket recv error -> {e}", extra={'frontend': str(self.frontend)})
+            raise RetarusAPIError(f"Unknown error: {e}")
 
-    def on_open(self, ws):
-        logger.info(f"[{__parser__}]:get_logs: Connection established.", extra={'frontend': str(self.frontend)})
-
-    def on_message(self, ws, message):
-        ## WRITE TO FILE ##
-        self.write_to_file([self.format_log(json.loads(message))])
-
-    def on_error(self, ws, error):
-        logger.error(f"[{__parser__}]:get_logs: {error}", extra={'frontend': str(self.frontend)})
-
-    def on_close(self, ws, status_code, close_msg):
-        logger.info(f"[{__parser__}]:get_logs: Connection closed.", extra={'frontend': str(self.frontend)})
+        if self.buffer:
+            logger.debug(f"[{__parser__}]:execute: flushing remaining logs", extra={'frontend': str(self.frontend)})
+            self._flush_buffer()
