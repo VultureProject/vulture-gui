VultureProject/vulture-gui

commit 948666dcfccbfab92c22c2c79a746d550dee9378

CLUSTER::CHANGED:: Refacto of code to correctly handle tasks before shutting down (#402)
### Changed
- [CLUSTER] Evolve daemon's code to allow correctly handling tasks during service shutdowns

vulture_os/daemons/tasks.py
@@ -1,0 +1,79 @@
+#!/home/vlt-os/env/bin/python
+"""This file is part of Vulture OS.
+
+Vulture OS is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Vulture OS is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Vulture OS.  If not, see http://www.gnu.org/licenses/.
+"""
+__author__ = "Théo Bertin"
+__credits__ = [""]
+__license__ = "GPLv3"
+__version__ = "4.0.0"
+__maintainer__ = "Vulture OS"
+__email__ = "contact@vultureproject.org"
+__doc__ = 'Vultured tasks and jobs'
+
+
+# Django system imports
+from django.conf import settings
+
+# Django project imports
+from system.cluster.models import Cluster
+
+# Extern modules imports
+from threading import Thread, Event
+
+# Logger configuration imports
+import logging
+logging.config.dictConfig(settings.LOG_SETTINGS)
+logger = logging.getLogger('daemon')
+
+
+def execute_tasks(node, count=1):
+    tasks = node.get_pending_messages(count=count)
+
+    for task in tasks:
+        status, result = task.execute()
+        logger.debug(f"TaskJob::execute_tasks: task {task.action} results are ({status}, '{result}')")
+
+
+class TasksJob(Thread):
+
+    def __init__(self, delay, **kwargs):
+        super().__init__(**kwargs)
+        # The shutdown_flag is a threading.Event object that
+        # indicates whether the thread should be terminated.
+        self.shutdown_flag = Event()
+        self.delay = delay
+
+    def run(self):
+        logger.info("Tasks job started.")
+        node = None
+
+        # While we are not asked to terminate
+        while not self.shutdown_flag.wait(self.delay):
+            if not node:
+                node = Cluster.get_current_node()
+                if not node:
+                    logger.error(f"Cluster::tasks: Could not get local Node configuration")
+                    continue
+            try:
+                execute_tasks(node)
+            except Exception as e:
+                logger.exception("Tasks job failure: {}".format(e))
+                logger.info("Resuming ...")
+
+        logger.info("Tasks job finished.")
+
+    def stop(self):
+        logger.info("Tasks shutdown asked!")
+        self.shutdown_flag.set()

vulture_os/daemons/cluster.py
@@ -15,7 +15,7 @@ You should have received a copy of the GNU General Public License
 along with Vulture 3.  If not, see http://www.gnu.org/licenses/.
 """
 __author__ = "Jérémie JOURDIN"
-__credits__ = ["Kevin GUILLEMOT"]
+__credits__ = ["Kevin GUILLEMOT", "Théo Bertin"]
 __license__ = "GPLv3"
 __version__ = "4.0.0"
 __maintainer__ = "Vulture OS"
@@ -39,84 +39,64 @@ import logging
 logging.config.dictConfig(settings.LOG_SETTINGS)
 logger = logging.getLogger('daemon')
 
-from system.cluster.models import Cluster
-from services.pf.pf import PFService
 from daemons.monitor import MonitorJob
-from services.exceptions import ServiceExit
+from daemons.tasks import TasksJob
 from signal import signal, SIGTERM, SIGINT
+from threading import Event
 
+SERVICE_SHUTDOWN = Event()
+JOB_DEFINITIONS = {
+    'tasks': {
+        'function': TasksJob,
+        'frequency': 5,
+    },
+    'monitor': {
+        'function': MonitorJob,
+        'frequency': 10,
+    }
+}
+RUNNING_JOBS = dict()
 
-def service_shutdown(signum, frame):
-    print('Caught signal %d' % signum)
-    raise ServiceExit
+def service_shutdown(signum, _):
+    logger.debug(f'Caught signal {signum}')
+    SERVICE_SHUTDOWN.set()
+    for jobname, job in RUNNING_JOBS.items():
+        logger.debug(f"asking {jobname} to stop...")
+        job.stop()
+
 
 
 """ This is for the cluster daemon process """
 if __name__ == '__main__':
-    """ Launch monitor job """
-    monitor_job = MonitorJob(10)
-    monitor_job.start()
+    """ Launch jobs """
+    SERVICE_SHUTDOWN.clear()
+    logger.info("Vultured started.")
+
+    for jobname, jobdef in JOB_DEFINITIONS.items():
+        logger.info(f"Vultured:: starting job {jobname}")
+        job = jobdef['function'](jobdef['frequency'], name=jobname)
+        job.start()
+        RUNNING_JOBS[jobname] = job
 
     signal(SIGTERM, service_shutdown)
     signal(SIGINT, service_shutdown)
 
-    error = False
-    this_node = None
+    while not SERVICE_SHUTDOWN.wait(5):
+        for jobname, job in RUNNING_JOBS.items():
+            if not job.is_alive():
+                logger.warning(f"Vultured:: job {jobname} crashed, relauching...")
+                try:
+                    jobdef = JOB_DEFINITIONS[jobname]
+                    job.join()
+                    job = jobdef['function'](jobdef['frequency'], name=jobname)
+                    job.start()
+                    RUNNING_JOBS[jobname] = job
+                except Exception as e:
+                    logger.error(f"Vultured:: Failed restarting job {jobname}!")
+                    continue
 
-    """ Continue as a daemon """
-    while True:
-        try:
-            """ May be False for pending new nodes """
-            if this_node:
-
-                # Process messages FIRST
-                """ Process inter-cluster messages """
-                this_node.process_messages()
-
-                if error:
-                    logger.info("Cluster::daemon: Recovered from previous failure")
-                    error = False
-            else:
-                this_node = Cluster.get_current_node()
-
-            time.sleep(5)
-
-        except ServiceExit as e:
-            """ Exiting asked """
-            logger.info("Cluster::daemon: Exit asked.")
-            break
-        except Exception as e:
-            # Try to log, in case of Mongodb failure
-            try:
-                logger.error("Cluster::daemon: General failure: {}".format(str(e)))
-            except ServiceExit:
-                """ Exiting asked """
-                print("Cluster::daemon: Exit asked.")
-                break
-            except Exception as e:
-                print("Cluster::daemon: General failure: {}".format(str(e)))
-
-            try:
-                pf = PFService()
-                stats = pf.get_rules()
-                # If PF enabled and 0 rules loaded
-                if len(stats) == 0:
-                    logger.info("Cluster::daemon: Loading pf rules")
-                    # Reload PF with conf file - in case of boot issue (e.g: domain in config file)
-                    pf.reload()
-            except ServiceExit:
-                print("Cluster::daemon: Exit asked.")
-                break
-            except Exception as e:
-                logger.error("Cluster::daemon: Failed to load PF rules : {}".format(str(e)))
-
-            time.sleep(5)
-            logger.info("Cluster::daemon: Trying to resume...")
-            error = True
-            this_node = None
-            continue
-
-    # Ask the jobs to terminate.
-    monitor_job.stop()
+    logger.info("Vultured stopping...")
+    for job in RUNNING_JOBS.values():
+        job.join()
 
     logger.info("Vultured stopped.")

vulture_os/daemons/monitor.py
@@ -51,9 +51,8 @@ from services.exceptions import ServiceError
 from django.core.exceptions import ObjectDoesNotExist
 
 # Extern modules imports
-from datetime import datetime, timedelta
+from datetime import timedelta
 from threading import Thread, Event
-from time import sleep
 
 # Logger configuration imports
 import logging
@@ -71,7 +70,7 @@ def monitor():
     logger.debug(f"Node state was: {node.state} {node.heartbeat}")
 
     def get_service_status(service_class):
-        """ Get a service_class (eg HaproxyService) 
+        """ Get a service_class (eg HaproxyService)
         :return  a dict {'name':service_name, 'status': status} """
         service_inst = service_class()
         service_status = ServiceStatus.objects.filter(name=service_inst.service_name).first() \
@@ -88,7 +87,7 @@ def monitor():
     )
     mon.services_id = set()
 
-    for service in [HaproxyService, DarwinService, PFService, 
+    for service in [HaproxyService, DarwinService, PFService,
                     StrongswanService, OpenvpnService, RsyslogService, FilebeatService]:
 
         # Get some statuses outside for reusing variable later
@@ -252,8 +251,8 @@ def monitor():
 
 class MonitorJob(Thread):
 
-    def __init__(self, delay):
-        super().__init__()
+    def __init__(self, delay, **kwargs):
+        super().__init__(**kwargs)
         # The shutdown_flag is a threading.Event object that
         # indicates whether the thread should be terminated.
         self.shutdown_flag = Event()
@@ -270,9 +269,8 @@ class MonitorJob(Thread):
                 logger.exception("Monitor job failure: {}".format(e))
                 logger.info("Resuming ...")
 
-        logger.info("Monitor job stopped.")
+        logger.info("Monitor job finished.")
 
     def stop(self):
-        logger.info("Monitor shutdown asked !")
+        logger.info("Monitor shutdown asked!")
         self.shutdown_flag.set()
-        self.join()

vulture_os/system/cluster/models.py
@@ -473,6 +473,15 @@ class Node(models.Model):
 
         return result
 
+    def get_pending_messages(self, count=None):
+        try:
+            return MessageQueue.objects.filter(
+            node=self, status__in=[MessageQueue.MessageQueueStatus.NEW, MessageQueue.MessageQueueStatus.RUNNING]).order_by('modified')[0:count]
+
+        except Exception as e:
+            logger.error(f"Could not get list of pending messages: {str(e)}")
+            return []
+
     def process_messages(self):
         """
         Function called from Cluster daemon: read
@@ -488,7 +497,7 @@ class Node(models.Model):
                 message.action, message.config, message.node)
             )
 
-            message.status = 'running'
+            message.status = MessageQueue.MessageQueueStatus.RUNNING
             message.save()
 
             try:
@@ -503,18 +512,18 @@ class Node(models.Model):
                     args.append(message.config)
 
                 message.result = my_function(*args)
-                message.status = 'done'
+                message.status = MessageQueue.MessageQueueStatus.DONE
             except ServiceExit as e:
                 """ Service stop asked """
                 raise e
             except KeyError as e:
                 logger.exception(e)
                 message.result = "KeyError {}".format(str(e))
-                message.status = 'failure'
+                message.status = MessageQueue.MessageQueueStatus.FAILURE
             except Exception as e:
                 logger.exception(e)
                 logger.error("Cluster::process_messages: {}".format(str(e)))
-                message.status = 'failure'
+                message.status = MessageQueue.MessageQueueStatus.FAILURE
                 message.result = str(e)
 
             message.save()
@@ -681,15 +690,24 @@ class Cluster (models.Model):
             return {'status': True, 'message': '', 'instance': m}
 
 
-class MessageQueue (models.Model):
+class MessageQueue(models.Model):
     """
         Cluster Queue
     """
+
+    class MessageQueueStatus(models.TextChoices):
+        NEW = "new", _("New")
+        RUNNING = "running", _("Running")
+        DONE = "done", _("Done")
+        FAILURE = "failure", _("Failure")
+
     date_add = models.DateTimeField(default=timezone.now)
     node = models.ForeignKey(Node, on_delete=models.CASCADE)
 
     # status of the action (new/running/done)
-    status = models.TextField(default='new')
+    status = models.TextField(
+        choices=MessageQueueStatus.choices,
+        default=MessageQueueStatus.NEW)
 
     result = models.TextField(default="")
 
@@ -721,6 +739,35 @@ class MessageQueue (models.Model):
         self.modified = timezone.now()
         return super().save(*args, **kwargs)
 
+
+    def execute(self):
+        self.status = MessageQueue.MessageQueueStatus.RUNNING
+        self.save()
+
+        try:
+            """ Big try in case of import or execution error """
+
+            # Call the function
+            my_function = import_string(self.action)
+            args = list()
+
+            args.append(logging.getLogger('daemon'))
+
+            if self.config:
+                args.append(self.config)
+
+            self.result = my_function(*args)
+            self.status = MessageQueue.MessageQueueStatus.DONE
+        except Exception as e:
+            logger.exception(e)
+            logger.error("MessageQueue::execute: {}".format(str(e)))
+            self.status = MessageQueue.MessageQueueStatus.FAILURE
+            self.result = str(e)
+
+        self.save()
+        return self.status, self.result
+
+
     def await_result(self, interval=2, tries=10):
         """
 
@@ -735,9 +782,9 @@ class MessageQueue (models.Model):
         try:
             for _ in range(0, tries):
                 message_instance = MessageQueue.objects.get(pk=self.id)
-                if message_instance.status == "done":
+                if message_instance.status == MessageQueue.MessageQueueStatus.DONE:
                     return True, message_instance.result
-                if message_instance.status == "failure":
+                if message_instance.status == MessageQueue.MessageQueueStatus.FAILURE:
                     return False, message_instance.result
                 time.sleep(interval)
 

vulture_os/system/migrations/0023_alter_messagequeue_status.py
@@ -1,0 +1,18 @@
+# Generated by Django 4.2.9 on 2024-02-13 09:33
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('system', '0022_alter_tlsprofile_protocols'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='messagequeue',
+            name='status',
+            field=models.TextField(choices=[('new', 'New'), ('running', 'Running'), ('done', 'Done'), ('failure', 'Failure')], default='new'),
+        ),
+    ]

commit f422862ff165d5990fd33a05e3e7ae6c9cde6c81

VULTURED::FIXED:: Remove regression in multiple tasks' execution speed introduced by 948666dc

vulture_os/daemons/tasks.py
@@ -38,14 +38,6 @@ logging.config.dictConfig(settings.LOG_SETTINGS)
 logger = logging.getLogger('daemon')
 
 
-def execute_tasks(node, count=1):
-    tasks = node.get_pending_messages(count=count)
-
-    for task in tasks:
-        status, result = task.execute()
-        logger.debug(f"TaskJob::execute_tasks: task {task.action} results are ({status}, '{result}')")
-
-
 class TasksJob(Thread):
 
     def __init__(self, delay, **kwargs):
@@ -67,7 +59,12 @@ class TasksJob(Thread):
                     logger.error(f"Cluster::tasks: Could not get local Node configuration")
                     continue
             try:
-                execute_tasks(node)
+                tasks = node.get_pending_messages(count=1)
+                while not self.shutdown_flag.is_set() and tasks:
+                    for task in tasks:
+                        status, result = task.execute()
+                        logger.debug(f"TaskJob::execute_tasks: task {task.action} results are ({status}, '{result}')")
+                    tasks = node.get_pending_messages(count=1)
             except Exception as e:
                 logger.exception("Tasks job failure: {}".format(e))
                 logger.info("Resuming ...")