VultureProject/vulture-gui

commit ec66749676c8da48e2cbb0d029d4f79e10d1871c

[REDIS] Access redis with authentication (#388)
### Added
- [FRONTEND] Allow to simply use local Redis without worrying about parameters when creating a Redis Listener
- [REDIS] Password support for local redis
- [REDIS] New api function set_password and set_replica_of
### Changed
- [API] Increase API key length for new clusters from 16 to 32 characters
- [INSTALL] Improve and stabilize the Redis bootstrapping process
- [CONFIG] [API] Remove the object ID from the API: there is only one configuration object!

vulture_os/system/config/models.py
@@ -58,6 +58,13 @@ class Config(models.Model):
     oauth2_header_name = models.TextField(blank=False, null=False, default='X-Vlt-Token')
     portal_cookie_name = models.TextField(blank=False, null=False, default='changeme')
     public_token = models.TextField(blank=False, null=False, default='changeme')
+    redis_password = models.TextField(
+        blank=True,
+        null=True,
+        default="",
+        verbose_name=_("Redis cluster password"),
+        help_text=_("Set the password for local Redis cluster")
+    )
     ldap_repository = models.ForeignKey(to=LDAPRepository, null=True, blank=False, on_delete=models.SET_NULL)
     branch = models.TextField(default="community")
     smtp_server = models.TextField(blank=True, default="")

vulture_os/services/frontend/models.py
@@ -467,7 +467,7 @@ class Frontend(models.Model):
         verbose_name=_("Redis server to use")
     )
     redis_port = models.PositiveIntegerField(
-        default="6379",
+        default=6379,
         help_text=_("Default redis port is 6379"),
         verbose_name=_("Redis port to use")
     )

vulture_os/system/cluster/models.py
@@ -302,7 +302,7 @@ class Node(models.Model):
         """
 
         if self.management_ip:
-            c = RedisBase()
+            c = RedisBase(password=Cluster.get_global_config().redis_password)
             master_node = c.get_master(self.name)
             if master_node == self.name:
                 return True
@@ -550,7 +550,7 @@ class Node(models.Model):
         return self._vstate
 
 
-class Cluster (models.Model):
+class Cluster(models.Model):
     """
     Vulture Cluster class.
 
@@ -586,7 +586,7 @@ class Cluster (models.Model):
 
         return global_config
 
-
+    @staticmethod
     def await_api_request(action, config=None, node=None, internal=False, interval=2, tries=10):
         """
         Call the usual api_request(), but wait for some time for a result before returning
@@ -630,7 +630,6 @@ class Cluster (models.Model):
                 })
 
             return status, results
-
 
     @staticmethod
     def api_request(action, config=None, node=None, internal=False):

vulture_os/daemons/monitor.py
@@ -207,7 +207,6 @@ def monitor():
         # If there is no VPNSSL conf on that node, pass
         pass
     else:
-
         openvpn.tunnels_status = get_ssl_tunnels_stats()
         openvpn.status = openvpn_status.status
         openvpn.save()

vulture_os/gui/static/js/listener.js
@@ -182,6 +182,21 @@ $(function() {
     });
   }
 
+  function refresh_redis_local_use() {
+    if (  $('#id_redis_server').val() === redis_local.server
+          && parseInt($('#id_redis_port').val()) === redis_local.port
+          && $('#id_redis_password').val() === redis_local.password
+        ) {
+      $('#id_redis_use_local').prop('checked', true).trigger('change');
+    }
+
+    if ($("#id_redis_use_local").is(':checked')) {
+      $('.redis-connect').hide();
+    } else {
+      $('.redis-connect').show();
+    }
+  }
+
   $('#stock_logs_locally').on('change', function(){
     var selected_forwarders = $('#id_log_forwarders').val();
 
@@ -260,6 +275,7 @@ $(function() {
       $('.kafka-mode').hide();
       // ALWAYS put show at last
       $('.redis-mode').show();
+      refresh_redis_local_use();
     } else {
       $('.file-mode').hide();
       $('.api-mode').hide();
@@ -436,6 +452,27 @@ $(function() {
     if ($(this).val() !== "" && $('#id_redis_mode').val() === "stream") {
       $('.redis-consumer-group').show();
     } else $('.redis-consumer-group').hide();
+  }).trigger('change');
+
+  var last_redis_server = $('#id_redis_server').val();
+  var last_redis_port = $('#id_redis_port').val();
+  var last_redis_password = $('#id_redis_password').val();
+
+  $('#id_redis_use_local').on('change', function(event) {
+    if ($(this).is(':checked')) {
+      last_redis_server = $('#id_redis_server').val();
+      last_redis_port = $('#id_redis_port').val();
+      last_redis_password = $('#id_redis_password').val();
+      $('#id_redis_server').val(redis_local.server);
+      $('#id_redis_port').val(redis_local.port);
+      $('#id_redis_password').val(redis_local.password);
+      $('.redis-connect').hide();
+    } else {
+      $('#id_redis_server').val(last_redis_server);
+      $('#id_redis_port').val(last_redis_port);
+      $('#id_redis_password').val(last_redis_password);
+      $('.redis-connect').show();
+    }
   }).trigger('change');
 
   /* Show logging options if logging enabled */

vulture_os/gui/views/install.py
@@ -36,6 +36,7 @@ from toolkit.mongodb.mongo_base import MongoBase
 from toolkit.redis.redis_base import RedisBase
 from toolkit.system.secret_key import set_key
 from toolkit.system.rc import get_rc_config
+from redis import AuthenticationError, ResponseError
 import logging
 import requests
 import subprocess
@@ -137,11 +138,16 @@ def cluster_create(admin_user=None, admin_password=None):
     """ Generate random API Key for cluster management and NMAP """
     """ Config object can not exists yet """
     system_config = cluster.get_global_config()
-    system_config.cluster_api_key = get_random_string(
-        16, 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-_.,+')
-    system_config.portal_cookie_name = get_random_string(
-        8, 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789')
-    system_config.public_token = get_random_string(16, 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789')
+    if not system_config.cluster_api_key:
+        system_config.cluster_api_key = get_random_string(
+            32, 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-_.,+')
+    if not system_config.portal_cookie_name:
+        system_config.portal_cookie_name = get_random_string(
+            8, 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789')
+    if not system_config.public_token:
+        system_config.public_token = get_random_string(16, 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789')
+    if not system_config.redis_password:
+        system_config.redis_password = get_random_string(64, 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789')
     system_config.set_logs_ttl()
     system_config.save()
 
@@ -159,14 +165,24 @@ def cluster_create(admin_user=None, admin_password=None):
         user.save()
 
     time.sleep(5)
-    """ Tell local sentinel to monitor local redis server """
-    c = RedisBase(get_management_ip(), 26379)
+    """ Set local redis server password """
     try:
-        # It may fail if Redis / Sentinel is already configured
-        c.sentinel_monitor()
-    except Exception as e:
-        logger.error("Install::Sentinel monitor: Error: ")
-        logger.exception(e)
+        redis = RedisBase(get_management_ip(), 6379)
+        redis.redis.ping()
+    except AuthenticationError as e:
+        redis = RedisBase(get_management_ip(), 6379, password=system_config.redis_password)
+    finally:
+        redis.set_password(system_config.redis_password)
+
+    """ Tell local sentinel to monitor local redis server """
+    sentinel = RedisBase(get_management_ip(), 26379)
+    try:
+        # It may fail if Sentinel is already configured
+        sentinel.sentinel_monitor()
+    except ResponseError:
+        logger.info(f"Install::Sentinel monitor: Monitoring cluster already configured")
+    sentinel.sentinel_set_announce_ip()
+    sentinel.sentinel_set_cluster_password(system_config.redis_password)
 
     """ Update uri of internal Log Forwarder """
     logfwd = LogOMMongoDB.objects.get()
@@ -235,7 +251,7 @@ def cluster_join(master_hostname, master_ip, secret_key, ca_cert=None, cert=None
             logger.info('Nodes not at the same date. Please sync with NTP Server')
             print('Nodes not at the same date. Please sync with NTP Server')
             return False
-        
+
         logger.info("[-] OK!")
 
     except Exception as e:
@@ -341,8 +357,7 @@ def cluster_join(master_hostname, master_ip, secret_key, ca_cert=None, cert=None
         infos = requests.post(
             "https://{}:8000/api/system/cluster/add/".format(master_ip),
             headers={'Cluster-api-key': secret_key},
-            # TODO will need to replace 'slave_ip' and 'slave_name' by 'ip' and 'name' resp.
-            data={'slave_ip': get_management_ip(), 'slave_name': get_hostname()},
+            data={'ip': get_management_ip(), 'name': get_hostname()},
             verify=False
         )
 
@@ -356,31 +371,6 @@ def cluster_join(master_hostname, master_ip, secret_key, ca_cert=None, cert=None
 
     except Exception:
         logger.error("Error at API Call on /system/cluster/add/ Response code: {}".format(infos.status_code))
-        return False
-
-    try:
-        """ Join our redis server to the redis master """
-        logger.info("[+] Making local Redis join the existing cluster")
-        c = RedisBase()
-        redis_master_node = None
-        retries = 0
-        while not redis_master_node and retries < 5:
-            logger.info("Getting current redis master from main node...")
-            redis_master_node = c.get_master(master_ip)
-            if not redis_master_node:
-                time.sleep(5)
-
-        logger.info(f"Current Redis master is {redis_master_node}, linking local redis to it")
-        c.replica_of(redis_master_node, 6379)
-
-        """ Tell local sentinel to monitor local redis server """
-        c = RedisBase(get_management_ip(), 26379)
-        c.sentinel_monitor(node=redis_master_node)
-
-        logger.info("[-] Ok!")
-
-    except Exception as e:
-        logger.error(f"Could not synchronize Redis instances: {e}")
         return False
 
     """ Sleep a few seconds in order for the replication to occur """

vulture_os/portal/system/redis_sessions.py
@@ -31,6 +31,7 @@ from django.utils.crypto            import get_random_string
 # Django project imports
 from authentication.base_repository import BaseRepository
 from authentication.learning_profiles.models import LearningProfile
+from system.cluster.models import Cluster
 
 # Required exceptions imports
 from .exceptions                     import TokenNotFoundError, REDISWriteError
@@ -664,8 +665,13 @@ class REDISBase(object):
         super(REDISBase, self).__init__()
 
         try:
-            #self.r = Redis(host=self.ip, port=self.port, db=0)
-            self.r = Redis(unix_socket_path='/var/sockets/redis/redis.sock', db=0, decode_responses=True)
+            self.r = Redis(
+                host='127.0.0.5', # Haproxy load-balancing
+                port=6379,
+                password=Cluster.get_global_config().redis_password,
+                db=0,
+                decode_responses=True
+            )
             self.r.info()
         except Exception as e:
             self.logger.info("REDISBase: REDIS connexion issue")

vulture_os/services/config/haproxy.conf
@@ -29,11 +29,7 @@ global
 
     nbthread 4
 
-    lua-load /usr/local/share/haproxy/lua/cors/cors.lua
-    lua-load /usr/local/share/haproxy/lua/session/session.lua
-
     ssl-default-bind-ciphers ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-SHA384:ECDHE-RSA-AES256-SHA384:ECDHE-ECDSA-AES128-SHA256:ECDHE-RSA-AES128-SHA256
-
 
 defaults
     mode http

commit 1b990f468fcb69fd9f8484f78cf24d756d2b7ba7

INSTALL::FIXED:: Regression introduced by ec667496
random values were left at 'changeme'

vulture_os/system/config/models.py
@@ -54,10 +54,10 @@ class Config(models.Model):
 
     pf_ssh_restrict = models.TextField(blank=False, null=False, default='any')
     pf_admin_restrict = models.TextField(blank=False, null=False, default='any')
-    cluster_api_key = models.TextField(blank=False, null=False, default='changeme')
+    cluster_api_key = models.TextField(blank=False, null=False, default='')
     oauth2_header_name = models.TextField(blank=False, null=False, default='X-Vlt-Token')
-    portal_cookie_name = models.TextField(blank=False, null=False, default='changeme')
-    public_token = models.TextField(blank=False, null=False, default='changeme')
+    portal_cookie_name = models.TextField(blank=False, null=False, default='')
+    public_token = models.TextField(blank=False, null=False, default='')
     redis_password = models.TextField(
         blank=True,
         null=True,