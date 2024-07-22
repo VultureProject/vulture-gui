a23fa34c2a1eb6ceb10ba0dfdb32d3a82bba201f
mbonniot <mael.bonniot@advens.fr>
2024-07-22 14:20:26 +0200

    [API_COLLECTOR][NOZOMI] Fix review suggested changes

--- vulture_os/toolkit/api_parser/nozomi_probe/nozomi_probe.py
+++ vulture_os/toolkit/api_parser/nozomi_probe/nozomi_probe.py
@@ -71,7 +71,7 @@ class NozomiProbeParser(ApiParser):
 
     def test(self):
         try:
-            status, logs = self.get_logs(to=timezone.now(), since=(timezone.now()-timedelta(days=1)))
+            status, logs = self.get_logs(since=(timezone.now()-timedelta(days=1)), to=timezone.now())
 
             return {
                 "status": status,
@@ -118,7 +118,7 @@ class NozomiProbeParser(ApiParser):
 
     def execute(self):
         since = self.last_api_call or timezone.now()-timedelta(days=30)
-        to = timezone.now() - timedelta(minutes=1)
+        to = min(timezone.now() - timedelta(minutes=1), since + timedelta(hours=24))
         cpt = 0
         total = 1
         while cpt < total:
