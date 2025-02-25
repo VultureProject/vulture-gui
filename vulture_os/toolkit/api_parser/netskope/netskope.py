61ff7ab7274723d83da12a8f24b1fd5d31a43e84
Gaultier Parain <104917501+gparain@users.noreply.github.com>
2025-02-25 15:35:07 +0100

    [API_PARSER][NETSKOPE] Fix last_collected_timestamp update when there is no log (#496)
    ### Fixed
    - [API_PARSER] [NETSKOPE] Correctly update the last_collected_timestamp, even when no logs are received

--- vulture_os/toolkit/api_parser/netskope/netskope.py
+++ vulture_os/toolkit/api_parser/netskope/netskope.py
@@ -139,7 +139,7 @@ class NetskopeParser(ApiParser):
         if self.netskope_get_application_logs:
             logtypes.append("application")
         for logtype in logtypes:
-            since = self.last_collected_timestamps.get(logtype, self.last_api_call) or (timezone.now() - timedelta(hours=24))
+            since = self.last_collected_timestamps.get(logtype) or (timezone.now() - timedelta(hours=24))
             to = min(timezone.now(), since + timedelta(hours=24))
             offset = 0
             logs = []
@@ -178,9 +178,9 @@ class NetskopeParser(ApiParser):
                             extra={'frontend': str(self.frontend)})
                 self.last_collected_timestamps[logtype] = to
 
-            elif self.last_api_call < timezone.now() - timedelta(hours=24):
+            elif since < timezone.now() - timedelta(hours=24):
                 # If no logs where retrieved during the last 24hours,
                 # move forward 1h to prevent stagnate ad vitam eternam
-                self.last_collected_timestamps[logtype] = self.last_collected_timestamps[logtype] + timedelta(hours=1)
+                self.last_collected_timestamps[logtype] = since + timedelta(hours=1)
 
             logger.info(f"[{__parser__}]:execute: Parsing done.", extra={'frontend': str(self.frontend)})
