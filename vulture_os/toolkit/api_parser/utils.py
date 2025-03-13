a51e07d79a25997103e5384bd81d94df7d7f3568
Gaultier Parain <104917501+gparain@users.noreply.github.com>
2025-03-13 10:05:23 +0100

    [API_PARSER] New parser VARONIS (#499)
    Co-authored-by: Théo Bertin <theo.bertin@advens.fr>

--- vulture_os/toolkit/api_parser/utils.py
+++ vulture_os/toolkit/api_parser/utils.py
@@ -78,6 +78,7 @@ from toolkit.api_parser.sentinel_one_singularity_mobile.sentinel_one_singularity
 from toolkit.api_parser.catonetworks.catonetworks import CatonetworksParser
 from toolkit.api_parser.infoblox_threat_defense.infoblox_threat_defense import InfobloxThreatDefenseParser
 from toolkit.api_parser.beyondtrust_reportings.beyondtrust_reportings import BeyondtrustReportingsParser
+from toolkit.api_parser.varonis.varonis import VaronisParser
 
 logging.config.dictConfig(settings.LOG_SETTINGS)
 logger = logging.getLogger('gui')
@@ -136,6 +137,7 @@ PARSER_LIST = {
     "catonetworks": CatonetworksParser,
     "infoblox_threat_defense": InfobloxThreatDefenseParser,
     "beyondtrust_reportings": BeyondtrustReportingsParser,
+    "varonis": VaronisParser,
 }
 
 class ParserDoesNotExist(Exception):
