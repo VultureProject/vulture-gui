fa5c8dcf5d85bbdbbe46e0e601a529b06e339cf0
rlanglois <robin.langlois@advens.fr>
2025-04-08 10:43:14 +0200

    trendmicro_visionone :: Add connect method, requests session, prepare request method

--- vulture_os/toolkit/api_parser/trendmicro_visionone/trendmicro_visionone.py
+++ vulture_os/toolkit/api_parser/trendmicro_visionone/trendmicro_visionone.py
@@ -59,37 +59,42 @@ class TrendmicroVisiononeParser(ApiParser):
         self.trendmicro_visionone_audit_timestamp = data.get("trendmicro_visionone_audit_timestamp")
         self.trendmicro_visionone_oat_timestamp = data.get("trendmicro_visionone_oat_timestamp")
 
+        self.session = None
+
+    def _connect(self):
+        try:
+            if self.session is None:
+                self.session = requests.Session()
+                self.session.headers.update({'Authorization': 'Bearer ' + self.trendmicro_visionone_token})
+                self.session.proxies = self.proxies
+                self.session.verify = self.api_parser_verify_ssl
+            return True
+        except Exception as err:
+            raise TrendmicroVisionOneAPIError(err)
+
     def __execute_query(self, kind, since, to, timeout=30):
+        self._connect()
 
         items = []
         status = False
 
-        headers = {'Authorization': 'Bearer ' + self.trendmicro_visionone_token, 'TMV1-Filter': ""}
         max_retries = 3
         retry = max_retries
 
         query = {}
-        url_path = ""
         link = ""
-        has_next = False
 
-        while not status and retry > 0 and not self.evt_stop.is_set():
+        while not status and retry > 0:
             self.update_lock()
             try:
-                if not has_next:
+                if not link:
                     # We have to do this only if we're not using the next link
-                    if kind == "alerts":
-                        query, url_path = self._prepare_request_alerts(since, to)
-                    elif kind == "audit":
-                        query, url_path = self._prepare_request_auditlogs(since, to)
-                    elif kind == "oat":
-                        query, url_path = self._prepare_request_OAT(since, to)
-                        headers.update({'TMV1-Filter': "not (riskLevel eq 'info')"})
-
+                    query, url_path = self._prepare_request(kind, since, to)
                     link = self.URL_BASE + url_path
 
-                logger.info(f"[{__parser__}]:__execute_query: URL: {link} , params: {query}", extra={'frontend': str(self.frontend)})
-                r = requests.get(link, params=query, headers=headers,proxies=self.proxies, verify=self.api_parser_verify_ssl, timeout=timeout)
+                logger.info(f"[{__parser__}]:__execute_query: URL: {link} , params: {query}, retry : {retry}", extra={'frontend': str(self.frontend)})
+
+                r = self.session.get(link, params=query, timeout=timeout)
                 if r.status_code != 200:
                     status = False
                     logger.warning(f"Error on URL: {link} Status: {r.status_code} Reason/Content: {r.content}")
@@ -103,17 +108,17 @@ class TrendmicroVisiononeParser(ApiParser):
                 items.extend(request_json['items'])
 
                 if link := request_json.get('nextLink'):
+                    retry = max_retries
                     query = None
-                    has_next = True
+                    continue
                 else:
-                    has_next = False
                     status = True
             except requests.exceptions.ReadTimeout as e:
-                logger.exception(f"[{__parser__}]:__execute_query: {e}", extra={'frontend': str(self.frontend)})
-                has_next = False
+                logger.warning(f"[{__parser__}]:__execute_query: {e}", extra={'frontend': str(self.frontend)})
+                link = ""
                 to -= (to - since) / 2
                 if (to - since) < timedelta(seconds=MIN_QUERY_INTERVAL):
-                    logger.warning(
+                    logger.error(
                         f"[{__parser__}]:__execute_query: Failed on {kind} logs, time range too small between {since} and {to}",
                         extra={'frontend': str(self.frontend)})
                     break
@@ -125,54 +130,51 @@ class TrendmicroVisiononeParser(ApiParser):
             except Exception as e:
                 logger.warning(f"[{__parser__}]:execute: Failed on {kind} logs, between time of {since} and {to} : {e}",
                              extra={'frontend': str(self.frontend)})
-                logger.exception(f"[{__parser__}]:__execute_query: {e}", extra={'frontend': str(self.frontend)})
                 retry -= 1
 
         if not status and retry <= 0:
             logger.error(f"[{__parser__}]:execute: Failed on {kind} logs, max of {max_retries} retries exceeded",
                          extra={'frontend': str(self.frontend)})
 
-        return items, status, since, to
+        return items, status, to
 
-    def _prepare_request_alerts(self, since=None, to=None):
-        url_path = '/v3.0/workbench/alerts'
+    def _prepare_request(self, kind, since=None, to=None):
+        url_paths = {
+            'alerts': '/v3.0/workbench/alerts',
+            'audit': '/v3.0/audit/logs',
+            'oat': '/v3.0/oat/detections'
+        }
         start_time = since.strftime("%Y-%m-%dT%H:%M:%SZ")
         end_time = to.strftime("%Y-%m-%dT%H:%M:%SZ")
-        query = {
-            'startDateTime': start_time,
-            'endDateTime': end_time,
-            'dateTimeTarget': 'createdDateTime',
-            'orderBy': 'createdDateTime desc',
-            'top': 200
+        queries = {
+            'alerts': {
+                'startDateTime': start_time,
+                'endDateTime': end_time,
+                'dateTimeTarget': 'createdDateTime',
+                'orderBy': 'createdDateTime desc',
+                'top': 200
+            },
+            'audit': {
+                'startDateTime': start_time,
+                'endDateTime': end_time,
+                'orderBy': 'createdDateTime desc',
+                'labels': 'all',
+                'top': 200
+            },
+            'oat': {
+                'detectedStartDateTime': start_time,
+                'detectedEndDateTime': end_time,
+                'top': 10000
+            }
+        }
+        headers = {
+            'alerts': {'TMV1-Filter': ""},
+            'audit': {'TMV1-Filter': ""},
+            'oat': {'TMV1-Filter': "not (riskLevel eq 'info')"}
         }
 
-        return query, url_path
-
-    def _prepare_request_auditlogs(self, since=None, to=None):
-        url_path = '/v3.0/audit/logs'
-        start_time = since.strftime("%Y-%m-%dT%H:%M:%SZ")
-        end_time = to.strftime("%Y-%m-%dT%H:%M:%SZ")
-        query = {
-            'startDateTime': start_time,
-            'endDateTime': end_time,
-            'orderBy': 'createdDateTime desc',
-            'labels': 'all',
-            'top': 200
-        }
-
-        return query, url_path
-
-    def _prepare_request_OAT(self, since=None, to=None):
-        url_path = '/v3.0/oat/detections'
-        start_time = since.strftime("%Y-%m-%dT%H:%M:%SZ")
-        end_time = to.strftime("%Y-%m-%dT%H:%M:%SZ")
-        query = {
-            'detectedStartDateTime': start_time,
-            'detectedEndDateTime': end_time,
-            'top': 10000
-        }
-
-        return query, url_path
+        self.session.headers.update(headers[kind])
+        return queries[kind], url_paths[kind]
     
     @staticmethod
     def _format_logs(log):
@@ -181,15 +183,22 @@ class TrendmicroVisiononeParser(ApiParser):
     def execute(self):
         for kind in ["alerts", "oat", "audit"]:
 
+            # if this is the first time : retrieve data from two days ago
             since = getattr(self.frontend, f"trendmicro_visionone_{kind}_timestamp") or (
                         timezone.now() - timedelta(days=2))
-            to = min(timezone.now() - timedelta(minutes=5), since + timedelta(hours=24))
 
-            while to <= timezone.now() - timedelta(minutes=5) and not self.evt_stop.is_set():
+            # we don't want logs newer than 5 minutes ago, to let some time for VisionOne to index logs
+            # we are retrieving maximum 24 hours at once, to avoid the collector to run for too long
+
+            max_to = min(timezone.now() - timedelta(minutes=5), since + timedelta(hours=1))
+            to = max_to
+
+            # "to" may be modified in the loop by __execute_query if there are too many logs
+            while to <= max_to and not self.evt_stop.is_set():
                 logger.info(f"[{__parser__}]:execute: Parser gets {kind} logs from {since} to {to}", extra={'frontend': str(self.frontend)})
 
                 self.update_lock()
-                logs, status, since, to = self.__execute_query(kind, since, to)
+                logs, status, to = self.__execute_query(kind, since, to)
 
                 if status:
                     self.write_to_file([self._format_logs(log) for log in logs])
@@ -202,11 +211,12 @@ class TrendmicroVisiononeParser(ApiParser):
                         # If no logs where retrieved during the last 24hours,
                         # move forward 1h to prevent stagnate ad vitam eternam
                         setattr(self.frontend, f"trendmicro_visionone_{kind}_timestamp", current_kind_timestamp + timedelta(hours=1))
+                    self.frontend.save()
 
+                    # we are re-using the computed time range as a basis for the next query
                     last_time_range = to - since
                     since = to
                     to += last_time_range
-                    self.frontend.save()
                 else:
                     logger.error(
                         f"[{__parser__}]:execute: Failed on {kind} logs, error status received",
@@ -230,11 +240,11 @@ class TrendmicroVisiononeParser(ApiParser):
         detection_logs = list()
 
         try:
-            alerts, status, _, _ = self.__execute_query("alerts", start_time, end_time)
+            alerts, status, _ = self.__execute_query("alerts", start_time, end_time)
             if status:
-                auditlogs, status, _, _ = self.__execute_query("audit", start_time, end_time)
+                auditlogs, status, _ = self.__execute_query("audit", start_time, end_time)
             if status:
-                detection_logs, status, _, _ = self.__execute_query("oat", start_time_oat, end_time)
+                detection_logs, status, _ = self.__execute_query("oat", start_time_oat, end_time)
 
             return {
                 "status": status,
