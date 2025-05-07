e040a7d379bc28aeff565ec866d35e1430a0da24
mbonniot <mael.bonniot@advens.fr>
2025-05-07 16:07:54 +0200

    [HORNETSECURITY] Fix review

--- vulture_os/toolkit/api_parser/hornetsecurity/hornetsecurity.py
+++ vulture_os/toolkit/api_parser/hornetsecurity/hornetsecurity.py
@@ -25,10 +25,11 @@ __doc__ = 'Hornet Security API Parser toolkit'
 __parser__ = 'HORNETSECURITY'
 
 
-from requests.sessions import Session
+from requests import get, post
 from datetime import datetime, timedelta
-from json import dumps, loads
+from json import dumps, loads, JSONDecodeError
 from base64 import b64encode
+from urllib3.exceptions import HTTPError
 
 import logging
 from django.conf import settings
@@ -50,38 +51,32 @@ class HornetSecurityParser(ApiParser):
         self.hornetsecurity_app_id = data["hornetsecurity_app_id"]
         self.hornetsecurity_token = data["hornetsecurity_token"]
 
-        self.session = Session()
-
         self.HIERARCHY_GET = "https://cp.hornetsecurity.com/api/v0/hierarchy/"
         self.AUDITS_SEARCH = "https://cp.hornetsecurity.com/api/v0/auditing/_search/"
         self.EMAILS_SEARCH = "https://cp.hornetsecurity.com/api/v0/emails/_search/"
-
-        # Get hierarchy root
-        self.root_id = self.get_root_hierarchy()
 
 
     def get_root_hierarchy(self) -> str:
         """
         Get root hierarchy id to use it as an 'object_id' later to gather logs
         """
-        resp = self.session.get(
-            url = self.HIERARCHY_GET,
-            params = {"limit": 1, "offset": 0},
-            headers = {"Authorization": f"Token {self.hornetsecurity_token}"})
         try:
+            resp = get(
+                url = self.HIERARCHY_GET,
+                params = {"limit": 1, "offset": 0},
+                headers = {"Authorization": f"Token {self.hornetsecurity_token}"})
+            resp.raise_for_status()
             json_resp = resp.json()
-        except Exception as e:
-            raise HornetSecurityAPIError(f"Failed to decode json response - {resp.content} -- {e}")
-
-        hierarchy = json_resp.get('hierarchy', {})
-        if hierarchy[0] if hierarchy else None:
-            if hierarchy[0].get('id'):
-                return hierarchy[0]['id']
-
-        msg = f"Fail to get hierarchy id -- {resp.content}"
-        logger.exception(f"[{__parser__}][get_root_hierarchy]: {msg}", extra={'frontend': str(self.frontend)})
-        raise HornetSecurityAPIError(msg)
-
+            return json_resp['hierarchy'][0]['id']
+        except (TimeoutError, ConnectionError, HTTPError) as e:
+            logger.error(f"[{__parser__}][get_root_hierarchy]: Error while fetching root hierarchy: {e}", extra={'frontend': str(self.frontend)})
+            raise HornetSecurityAPIError("Error while querying Root Hierarchy")
+        except JSONDecodeError as e:
+            logger.error(f"[{__parser__}][get_root_hierarchy]: Could not decode json response: {e}", extra={'frontend': str(self.frontend)})
+            raise HornetSecurityAPIError("Failed to decode json response")
+        except (KeyError, TypeError, IndexError) as e:
+            logger.error(f"[{__parser__}][get_root_hierarchy]: Could not get valid ID from response: {e}", extra={'frontend': str(self.frontend)})
+            raise HornetSecurityAPIError("Could not get hierarchy ID from response")
 
     def execute_query(self, kind: str, since: datetime, to: datetime, limit: int, offset: int) -> tuple[str, int, list]:
         since = datetime.strftime(since, "%Y-%m-%dT%H:%M:%SZ")
@@ -91,52 +86,54 @@ class HornetSecurityParser(ApiParser):
         while retry > 0:
             try:
                 if kind == "auditing":
-                    resp = self.session.post(
-                        url = self.AUDITS_SEARCH,
-                        params = {"object_id": self.root_id},
-                        headers = {"APP-ID": self.hornetsecurity_app_id, "Authorization": f"Token {self.hornetsecurity_token}"},
-                        data = {"date_from": since, "date_to": to, "limit": limit, "offset": offset},
-                        timeout = 10)
+                    url = self.AUDITS_SEARCH
                 elif kind == "emails":
-                    resp = self.session.post(
-                        url = self.EMAILS_SEARCH,
-                        params = {"object_id": self.root_id},
-                        headers = {"APP-ID": self.hornetsecurity_app_id, "Authorization": f"Token {self.hornetsecurity_token}"},
-                        data = {"date_from": since, "date_to": to, "limit": limit, "offset": offset},
-                        timeout = 10)
+                    url = self.EMAILS_SEARCH
+                params = {"object_id": self.root_id}
+                data = {"date_from": since, "date_to": to, "limit": limit, "offset": offset}
 
-            except Exception as e:
+                logger.info(f"[{__parser__}][execute_query] Querying {url} with parameters {params} and data {data}")
+
+                resp = post(
+                    url = url,
+                    params = params,
+                    headers = {"APP-ID": self.hornetsecurity_app_id, "Authorization": f"Token {self.hornetsecurity_token}"},
+                    data = data,
+                    proxies=self.proxies,
+                    verify=self.api_parser_custom_certificate or self.api_parser_verify_ssl,
+                    timeout = 10)
+                
+                json_resp = resp.json()
+
+                if resp.status_code != 200:
+                    logger.exception(f"[{__parser__}][execute_query]: Receive {resp.status_code} != 200 while fetching {kind}'s logs - {since, to} -- {resp.content}", extra={'frontend': str(self.frontend)})
+                    retry -= 1
+                    continue
+
+            except TimeoutError as e:
                 logger.exception(f"[{__parser__}][execute_query]: Unexpected error while fetching {kind}'s logs - {since, to} -- {e}", extra={'frontend': str(self.frontend)})
                 retry -= 1
                 continue
-
-            try:
-                json_resp = resp.json()
-            except Exception as e:
+            except JSONDecodeError as e:
                 logger.exception(f"[{__parser__}][execute_query]: Failed to decode json response {resp.content} -- {e}", extra={'frontend': str(self.frontend)})
                 raise HornetSecurityAPIError(e)
 
             # Handles more than 10k logs to retrieve in a single query
             toRetrieve = json_resp.get('num_found_items', 0)
             if toRetrieve >= 10000: # error_id returned by API in this case is 3089
-                return "divide", toRetrieve, []
-
-            if resp.status_code != 200:
-                logger.exception(f"[{__parser__}][execute_query]: Receive {resp.status_code} != 200 while fetching {kind}'s logs - {since, to} -- {resp.content}", extra={'frontend': str(self.frontend)})
-                retry -= 1
-                continue
+                return toRetrieve, []
 
             if kind == "auditing":
-                return "write", toRetrieve, json_resp.get('auditings', [])
+                return toRetrieve, json_resp.get('auditings', [])
             elif kind == "emails":
-                return "write", toRetrieve, json_resp.get('emails', [])
+                return toRetrieve, json_resp.get('emails', [])
 
         msg = f"Fail to fetch {kind} by encountering more than 3 errors consecutively {since, to}"
         logger.exception(f"[{__parser__}][execute_query]: {msg}", extra={'frontend': str(self.frontend)})
         raise HornetSecurityAPIError(msg)
 
 
-    def get_logs(self, kind: str, since: datetime, to: datetime) -> tuple[list, datetime]:
+    def get_logs(self, kind: str, initial_since: datetime, initial_to: datetime) -> tuple[list, datetime]:
         """
         Get logs by kind specification and (since; to) couple
         If there is more than 10000 logs to retrieve, the collector continuously divide the range of time to get by two; reducing probability to encounters more than the limit number of logs
@@ -145,29 +142,26 @@ class HornetSecurityParser(ApiParser):
             -> by default logs are timestamp descendently sorted (thus we always retrieve the max serie timestamp on the first chunk's log)
         """
         # API doesn't supports microseconds precision we must remove it
-        since = since.replace(microsecond=0)
-        to = to.replace(microsecond=0)
+        since = initial_since.replace(microsecond=0)
+        to = initial_to.replace(microsecond=0)
 
         ret = []
+        count = 1 # to trigger first loop execution
 
         limit = 1000
-        op, count, logs = self.execute_query(kind, since, to, limit, len(ret))
-        if op == "divide":
-            to = since + ((to - since) / 2)
-            logger.info(f"[{__parser__}][get_logs]: We've tried to retrieve more than 10k {kind} in one query ({count}) - dividing the range of time by 2 -> {since.isoformat(), to.isoformat()}", extra={'frontend': str(self.frontend)})
-        elif op == "write":
-            ret.extend(logs)
-
-        while (len(ret) != count and not self.evt_stop.is_set()):
-            op, count, logs = self.execute_query(kind, since, to, limit, len(ret))
-            if op == "divide":
+        while (len(ret) < count and not self.evt_stop.is_set()):
+            count, logs = self.execute_query(kind, since, to, limit, len(ret))
+            if count >= 10000:
                 to = since + ((to - since) / 2)
                 logger.info(f"[{__parser__}][get_logs]: We've tried to retrieve more than 10k {kind} in one query ({count}) - dividing the range of time by 2 -> {since.isoformat(), to.isoformat()}", extra={'frontend': str(self.frontend)})
                 continue
-            elif op == "write":
+            elif count > 0:
                 ret.extend(logs)
 
             self.update_lock()
+
+        if self.evt_stop.is_set() and ret != count: # partial logs retrieved -> we must return with an unmodified to
+            return [], initial_to.replace(microsecond=0)
 
         return ret, to
 
@@ -180,16 +174,14 @@ class HornetSecurityParser(ApiParser):
         ## EMAILS
         if kind == "emails":
             # size
-            size : dict = log.get('size')
+            size : dict = log.get('size', {})
             if size:
                 value : int = size.get('value', 0)
                 unit : str = size.get('unit', "kb")
                 unit = unit.lower()
 
                 unit_table = {"kb": 1000, "mb": 1000000, "gb": 1000000000}
-                size = value * unit_table.get(unit, 0)
-
-                log['size'] = size
+                log['size'] = value * unit_table.get(unit, 0)
 
             # attachments
             attachments : list = log.get('attachments', [])
@@ -205,13 +197,13 @@ class HornetSecurityParser(ApiParser):
             log['attachments'] = new_attachments
 
             # crypt_type_*
-            crypt_type_in_text : dict = log.get('crypt_type_in', {}).get('text')
+            crypt_type_in_text = log.get('crypt_type_in', {}).get('text')
             if isinstance(crypt_type_in_text, str):
                 log['crypt_type_in'] = crypt_type_in_text.lower()
             else:
                 log['crypt_type_in'] = "unknown"
 
-            crypt_type_out_text : dict = log.get('crypt_type_out', {}).get('text')
+            crypt_type_out_text = log.get('crypt_type_out', {}).get('text')
             if isinstance(crypt_type_out_text, str):
                 log['crypt_type_out'] = crypt_type_out_text.lower()
             else:
@@ -227,14 +219,14 @@ class HornetSecurityParser(ApiParser):
                 log['direction'] = "unknown"
 
             # classification
-            classification_text : dict = log.get('classification', {}).get('text')
+            classification_text = log.get('classification', {}).get('text')
             if isinstance(classification_text, str):
                 log['classification'] = classification_text.lower()
             else:
                 log['classification'] = "unknown"
 
             # status
-            status_text : dict = log.get('status', {}).get('text')
+            status_text = log.get('status', {}).get('text')
             if isinstance(status_text, str):
                 log['status'] = status_text.lower()
             else:
@@ -262,7 +254,9 @@ class HornetSecurityParser(ApiParser):
                 try:
                     log['subject'] = str(b64encode(subject.encode('utf-8')))
                     log['is_subject_encoded'] = True
-                except:
+                except ValueError or TypeError as e: # Failed to b64decode
+                    log['subject'] = ""
+                except UnicodeError as e: # Failed to decode utf-8
                     log['subject'] = ""
 
             # destination_hostname
@@ -277,7 +271,10 @@ class HornetSecurityParser(ApiParser):
             # new_values
             new_values = log.get('new_values')
             if new_values:
-                new_values = loads(new_values)
+                try:
+                    new_values = loads(new_values)
+                except JSONDecodeError as e:
+                    raise HornetSecurityAPIError(e)
                 if not isinstance(new_values, dict):
                     log['new_values'] = {}
                 else:
@@ -287,14 +284,19 @@ class HornetSecurityParser(ApiParser):
                         try:
                             new_values['subject'] = str(b64encode(subject.encode('utf-8')))
                             new_values['is_subject_encoded'] = True
-                        except:
-                            new_values['subject'] = ""
+                        except ValueError or TypeError as e: # Failed to b64decode
+                            log['subject'] = ""
+                        except UnicodeError as e: # Failed to decode utf-8
+                            log['subject'] = ""
                     log['new_values'] = new_values
 
             # old_values
             old_values = log.get('old_values')
             if old_values:
-                old_values = loads(old_values)
+                try:
+                    old_values = loads(old_values)
+                except JSONDecodeError as e:
+                    raise HornetSecurityAPIError(e)
                 if not isinstance(old_values, dict):
                     log['old_values'] = {}
                 else:
@@ -302,29 +304,30 @@ class HornetSecurityParser(ApiParser):
                     if isinstance(subject, str) and not is_ascii(subject):
                         old_values['is_subject_encoded'] = False
                         try:
-                            old_values['subject'] = str(b64encode(subject.encode('urd-8')))
+                            old_values['subject'] = str(b64encode(subject.encode('utf-8')))
                             old_values['is_subject_encoded'] = True
-                        except:
-                            old_values['subject'] = ""
+                        except ValueError or TypeError as e: # Failed to b64decode
+                            log['subject'] = ""
+                        except UnicodeError as e: # Failed to decode utf-8
+                            log['subject'] = ""
                     log['old_values'] = old_values
 
         return dumps(log)
 
 
     def execute(self):
-        for kind in ["auditing", "emails"]:
-            logs = []
+        # Get hierarchy root
+        self.root_id = self.get_root_hierarchy()
 
+        for kind in ["auditing", "emails"]:
             since = self.last_collected_timestamps.get(f"hornetsecurity_{kind}") or (timezone.now() - timedelta(days=7))
             to = min(timezone.now() - timedelta(minutes=3), since + timedelta(days=1))
 
             new_logs, new_to = self.get_logs(kind, since, to)
 
-            logger.info(f"[{__parser__}][execute]: Successfully get {len(new_logs)} {kind}'s logs from {since} to {new_to}", extra={'frontend': str(self.frontend)})
-            logs.extend(new_logs)
-
-            if logs:
-                self.write_to_file([self.format_log(kind, log) for log in logs])
+            logger.info(f"[{__parser__}][execute]: Successfully got {len(new_logs)} {kind}'s logs from {since} to {new_to}", extra={'frontend': str(self.frontend)})
+            if new_logs:
+                self.write_to_file([self.format_log(kind, log) for log in new_logs])
                 self.update_lock()
 
             # this incrementation is mandatory to not get doublons for every request - overlapping from one range to another - even if it's uggly and possibly causing logs missing
@@ -332,6 +335,9 @@ class HornetSecurityParser(ApiParser):
             logger.info(f"[{__parser__}][execute]: Updated last_collected_timestamps['hornetsecurity_{kind}'] to {new_to}", extra={'frontend': str(self.frontend)})
 
     def test(self):
+        # Get hierarchy root
+        self.root_id = self.get_root_hierarchy()
+
         try:
             logger.debug(f"[{__parser__}][test]:Running tests...", extra={'frontend': str(self.frontend)})
 
