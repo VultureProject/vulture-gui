54e6a0c713121b4636290d0d0e10900cc3d070a1
Gaultier PARAIN <gaultier.parain@advens.fr>
2025-05-20 16:18:13 +0200

    [API_PARSER][GATEWATCHER_ALERTS] Support api with the former endpoint

--- vulture_os/toolkit/api_parser/gatewatcher_alerts/gatewatcher_alerts.py
+++ vulture_os/toolkit/api_parser/gatewatcher_alerts/gatewatcher_alerts.py
@@ -44,6 +44,9 @@ class GatewatcherAlertsParser(ApiParser):
     ALERTS_ENDPOINT = "/api/v1/alerts/"
     RAW_ALERTS_ENDPOINT = "/api/v1/raw-alerts/"
 
+    DEPRECATED_ALERTS_ENDPOINT = "/api/alerts/"
+    DEPRECATED_RAW_ALERTS_ENDPOINT = "/api/raw-alerts/"
+
     HEADERS = {
         "Content-Type": "application/json",
         'Accept': 'application/json'
@@ -77,6 +80,18 @@ class GatewatcherAlertsParser(ApiParser):
             timeout=timeout,
             verify=self.api_parser_custom_certificate or self.api_parser_verify_ssl
         )
+        if response.status_code == 404:
+            logger.warning(f"[{__parser__}]:execute_query: 404 error at Gatewatcher API Call, content : {response.content}", extra={'frontend': str(self.frontend)})
+            # The cause of the 404 error can be that the former endpoint us used
+            url = url.replace("/api/", "/api/v1/")
+            response = self.session.get(
+                url,
+                params=params,
+                proxies=self.proxies,
+                timeout=timeout,
+                verify=self.api_parser_custom_certificate or self.api_parser_verify_ssl
+            )
+
         if response.status_code != 200:
             error = f"Error at Gatewatcher API Call: status : {response.status_code}, content : {response.content}"
             logger.error(f"[{__parser__}]:execute_query: {error}", extra={'frontend': str(self.frontend)})
