72ec387cbfa6223469f3ab13f129a2173e2cec78
Théo Bertin <theo.bertin@advens.fr>
2025-06-04 13:27:58 +0200

    change(tls): Filter available tls profiles for Listeners

--- vulture_os/services/frontend/form.py
+++ vulture_os/services/frontend/form.py
@@ -932,13 +932,17 @@ class FrontendForm(RsyslogQueueForm, ModelForm):
 
 class ListenerForm(ModelForm):
 
+    tls_profiles = ModelMultipleChoiceField(
+        queryset=TLSProfile.objects.filter(x509_certificate__isnull=False),
+        widget=SelectMultiple(attrs={'class': 'form-control select2'}),
+    )
+
     class Meta:
         model = Listener
         fields = ('network_address', 'port', 'tls_profiles', 'whitelist_ips', 'max_src', 'max_rate',)
 
         widgets = {
             'port': NumberInput(attrs={'class': 'form-control'}),
-            'tls_profiles': SelectMultiple(choices=TLSProfile.objects.all(), attrs={'class': 'form-control select2'}),
             'whitelist_ips': TextInput(attrs={'class': 'form-control', 'data-role': "tagsinput"}),
             'max_src': NumberInput(attrs={'class': 'form-control'}),
             'max_rate': NumberInput(attrs={'class': 'form-control'})
