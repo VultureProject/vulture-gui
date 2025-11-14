f20c3e34dc2d758ca46d2d6874d9fe78265ab812
mbonniot <mael.bonniot@advens.fr>
2025-11-14 10:05:28 +0100

    [VULTURE-GUI] [WORKFLOW] Remove djongo support

--- vulture_os/workflow/models.py
+++ vulture_os/workflow/models.py
@@ -27,7 +27,7 @@ __doc__ = 'Workflow model classes'
 from django.conf import settings
 from django.utils.translation import gettext_lazy as _
 from django.forms.models import model_to_dict
-from djongo import models
+from django.db import models
 from django.template import Context
 from django.template import Template
 
